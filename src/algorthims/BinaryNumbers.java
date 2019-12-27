00f2379c97777b9420278e758bd479f3ea361833
Davinder Singh Kharoud <dskharoud2@gmail.com>
2019-12-26 23:32:04 -0500

    Binary.

---
+++ src/algorthims/BinaryNumbers.java
@@ -0,0 +1,63 @@
+package algorthims;
+
+/*Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base- 10 integer
+denoting the maximum number of consecutive 1's in n's binary representation.*/
+public class BinaryNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println(getBinary(13));
+        System.out.println(getConsicutiveOne(getBinary(13)));
+
+
+    }
+
+    //Converting in to binary numbers
+    private static String getBinary(int num){
+        String binary = "";
+
+        while(num!=1){
+
+            if( num%2 == 0 ){
+
+                binary+="0";
+
+            }else{
+
+                binary+= "1";
+
+            }
+
+            num = num/2;
+        }
+
+        binary+="1";
+        return binary;
+
+    }
+
+    private static int getConsicutiveOne(String binary){
+        int finalCount=0;
+        int temp=0;
+
+        for(int i=0; i<binary.length(); i++){
+
+            if(binary.charAt(i) == '1'){
+
+                temp += 1;
+
+            } else {
+
+                temp = 0;
+            }
+
+            if(finalCount<temp){
+                finalCount = temp;
+            }
+
+
+        }
+        return finalCount;
+
+    }
+}
