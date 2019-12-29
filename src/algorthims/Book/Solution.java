069c2415f7cf9fd9afae4291dd1079a4a0d021d6
Davinder Singh Kharoud <dskharoud2@gmail.com>
2019-12-28 23:44:18 -0500

    MyBook Abstract.

---
+++ src/algorthims/Book/Solution.java
@@ -0,0 +1,18 @@
+package algorthims.Book;
+
+import java.util.Scanner;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
