db8c42229a6d04b21a38fdb3924b6470797e6aad
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-05 00:15:03 -0500

    Linked list (removeDuplicates)

--- src/algorthims/BinaryTreeHeight/BinaryTree.java
+++ src/algorthims/BinaryTreeHeight/BinaryTree.java
@@ -13,16 +13,30 @@ public class BinaryTree {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the number of nodes: ");
         int T=sc.nextInt();
         Node root=null;
         while(T-->0){
+            System.out.print("Enter the data: ");
             int data=sc.nextInt();
             root=insert(root,data);
         }
         int height=getHeight(root);
-        System.out.println(height);
+        System.out.println("Height of the tree: "+height);
+        inOrderTraversal(root);
     }
 
+    //InOrder Traversal
+    private static void inOrderTraversal(Node root){
+        if(root == null){
+            return;
+        }
+        inOrderTraversal(root.left);
+        System.out.println(root.data);
+        inOrderTraversal(root.right);
+
+    }
 
     private static Node insert(Node root,int data){
         if(root==null){
