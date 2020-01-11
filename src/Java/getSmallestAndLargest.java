2dedc404bf84942e88b05e23f378c787ce290678
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-11 10:44:44 -0500

    getSmallestAndLargest.

---
+++ src/Java/getSmallestAndLargest.java
@@ -0,0 +1,32 @@
+package Java;
+
+import java.lang.reflect.Array;
+
+public class getSmallestAndLargest {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        String []arr = new String[ s.length() - (k-1)];
+
+        for(int i = 0 ; i<s.length()-(k-1); i++){
+
+            arr[i] = s.substring(i, i+k);
+
+        }
+
+        java.util.Arrays.sort(arr);
+
+        smallest = arr[0];
+        largest = arr[arr.length-1];
+
+
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getSmallestAndLargest("welcometojava",3));
+    }
+}
