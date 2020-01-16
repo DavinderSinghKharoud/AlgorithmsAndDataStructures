1e1396e4ab04ecac0fe3f219b8df476cc4694994
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-15 19:50:06 -0500

    Bubble sort.

---
+++ src/SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,42 @@
+package SortingAlgorithms;
+
+public class BubbleSort {
+
+
+    private static void sort(int[] arr) {
+
+        for( int first = 0; first<arr.length; first++){
+
+            for( int index = 0; index<arr.length - 1; index++){
+
+                if( arr[index] > arr[index+1]){
+
+                    swap( arr, index, index+1);
+                }
+
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {4,3,2,1,3,2,4,4,5};
+
+        sort(arr);
+
+        for( int elem: arr){
+
+            System.out.println(elem);
+        }
+    }
+
+}
