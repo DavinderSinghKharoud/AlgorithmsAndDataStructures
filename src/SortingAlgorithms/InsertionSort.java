5d48a0872edeaee728917555ff183731c47bb82a
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-15 19:42:30 -0500

    Insertion sort.

---
+++ src/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,39 @@
+package SortingAlgorithms;
+
+public class InsertionSort {
+
+    private static void sort(int[] arr) {
+
+        int key, checkIndex;
+
+        for( int i = 1; i<arr.length; i++){
+
+            key = arr[i];
+            checkIndex = i - 1;
+
+            while( checkIndex>=0 && arr[checkIndex] > key){
+
+                arr[ checkIndex+1 ] = arr[checkIndex];
+                checkIndex = checkIndex - 1;
+            }
+
+            arr[ checkIndex + 1] = key;
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int []arr = {4,3,2,1,2,1,2,3,3};
+
+        sort(arr);
+
+        for( int elem: arr){
+
+            System.out.println(elem);
+        }
+    }
+
+
+}
