cffb16020e44c3345f08f801793e2103e0984e16
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-20 23:45:01 -0500

    Largest Prime Number.

---
+++ src/ProjectEuler/LargestPrimaryNumber.java
@@ -0,0 +1,30 @@
+package ProjectEuler;
+
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+public class LargestPrimaryNumber {
+
+    static long getLargestPrime( long num){
+
+        while ( num%2 == 0){
+            num /= 2;
+        }
+
+        for( long fac = 3; fac<num; fac+=2 ){
+
+            while ( num%fac == 0 && fac<num){
+
+                num/=fac;
+            }
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+
+        String num = "600851475143";
+        System.out.println(getLargestPrime( Long.parseLong( num ) ));
+    }
+}
