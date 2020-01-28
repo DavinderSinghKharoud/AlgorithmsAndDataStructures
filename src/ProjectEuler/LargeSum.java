8fa5bbc1cef955217b31ebcd6ab3c6d6f46c6651
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-01-28 00:10:44 -0500

    Largest Sum.

---
+++ src/ProjectEuler/LargeSum.java
@@ -0,0 +1,38 @@
+package ProjectEuler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LargeSum {
+
+    private static final int SIZE = 100;
+
+    public static void main(String[] args) {
+        BigInteger[] numbers = readData("C:\\Users\\SHARMA\\IdeaProjects\\AlgorithmsAndDataStructures\\src\\ProjectEuler\\euler12");
+        BigInteger sum = BigInteger.ZERO;
+        for(int i = 0; i < numbers.length; ++i)
+            sum = sum.add(numbers[i]);
+        String sumStr = sum.toString().substring(0,10);
+
+        System.out.println(sumStr + " is first ten digits.");
+    }
+
+    private static BigInteger[] readData(String filename) {
+        BigInteger[] nums = new BigInteger[SIZE];
+        try {
+            Scanner input = new Scanner(new File(filename));
+            for(int i = 0; input.hasNext(); i++) {
+                nums[i] = new BigInteger(input.next());
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("cannot find file " + filename);
+        }
+
+
+
+        return nums;
+    }
+}
