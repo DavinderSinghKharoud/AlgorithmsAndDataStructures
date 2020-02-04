de077a069690d4186cff4e3f320a9f852e9363b7
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-03 23:45:26 -0500

    Climbing Stairs.

--- src/algorthims/LeetCode/ClimbingStairs.java
+++ src/algorthims/LeetCode/ClimbingStairs.java
@@ -47,7 +47,7 @@ public class ClimbingStairs {
         dp[1] = 1;
         dp[2] = 2;
 
-        for(int i = 3; i<n; i++){
+        for(int i = 3; i<=n; i++){
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
