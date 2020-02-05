4f19535672573d68e0b0614a1c7fb0053c550fb3
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-04 23:58:31 -0500

    Diameter Of Binary Tree.

---
+++ src/algorthims/LeetCode/DiameterOfBinaryTree.java
@@ -0,0 +1,42 @@
+package algorthims.LeetCode;
+
+public class DiameterOfBinaryTree {
+
+    static int ans;
+
+    public static void main(String[] args) {
+
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        treeNode.left.left = new TreeNode(4);
+        treeNode.left.right = new TreeNode(5);
+
+        System.out.println( diameterOfBinaryTree( treeNode));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public static int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    public static int depth(TreeNode node) {
+        if (node == null) return 0;
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L + R + 1);
+        return Math.max(L, R) + 1;
+    }
+}
