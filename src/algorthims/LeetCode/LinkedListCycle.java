68eabbae7d140723d7c4e07783a33c44fb92626e
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-16 02:04:01 -0500

    Linked List Cycle.

--- src/algorthims/LeetCode/LinkedListCycle.java
+++ src/algorthims/LeetCode/LinkedListCycle.java
@@ -1,9 +1,14 @@
 package algorthims.LeetCode;
 
-
-import java.util.HashMap;
-import java.util.List;
-
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list,
+ * we use an integer pos which represents the position (0-indexed)
+ * in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+ *
+ *
+ */
 public class LinkedListCycle {
 
     public static void main(String[] args) {
