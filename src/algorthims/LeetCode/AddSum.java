b209ff929392c996c6fc8e6526b80a79de1c0232
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-19 10:51:13 -0500

    Add Sum(Hash map).

--- src/algorthims/LeetCode/AddSum.java
+++ src/algorthims/LeetCode/AddSum.java
@@ -1,6 +1,11 @@
 package algorthims.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddSum {
+
+    //( N ) Square
     public static int[] twoSum(int[] nums, int target) {
 
         int []result = new int[2];
@@ -23,11 +28,31 @@ public class AddSum {
         return result;
     }
 
+    //Hash table
+    public static int[] twoSumHash(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i =0; i<nums.length; i++){
+            int complement = target - nums[i];
+
+            if( map.containsKey(complement)){
+                return new int[] {map.get(complement), i};
+            }
+
+            map.put( nums[i], i);
+        }
+
+        return new int[] {};
+    }
     public static void main(String[] args) {
 
-        printArr( twoSum( new int[]{
+//        printArr( twoSum( new int[]{
+//                3,2,4
+//        },6));
+
+        printArr( twoSumHash( new int[]{
                 3,2,4
-        },6));
+        }, 6));
     }
 
     private static void  printArr( int[] nums){
