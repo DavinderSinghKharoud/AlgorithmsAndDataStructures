c337768e9470118fef21d42bc3f26a56fec35807
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-27 00:13:41 -0500

    Counting Bits(More Efficient)

--- src/algorthims/LeetCode/CountingBits.java
+++ src/algorthims/LeetCode/CountingBits.java
@@ -18,9 +18,16 @@ public class CountingBits {
         return res;
     }
 
-
+    //O(n)
+    public static int[] countBits1(int num) {
+        int arr[] = new int[num+1];
+        arr[0]=0;
+        for(int i=1;i<=num;i++)
+            arr[i] = i%2==0 ? arr[i/2] : arr[i/2]+1;
+        return arr;
+    }
     public static void main(String[] args) {
-        int [] result = countBits( 2 );
+        int [] result = countBits1( 2 );
 
         for( int num: result ){
             System.out.println(num);
