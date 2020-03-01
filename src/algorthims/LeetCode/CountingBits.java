84134f3df846a9fb654fc1147bbdc369b0e12217
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-29 23:42:30 -0500

    Counting Bits(More Efficient)

--- src/algorthims/LeetCode/CountingBits.java
+++ src/algorthims/LeetCode/CountingBits.java
@@ -1,5 +1,9 @@
 package algorthims.LeetCode;
 
+/**
+ * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num
+ * calculate the number of 1's in their binary representation and return them as an array.
+ */
 public class CountingBits {
 
     public static int[] countBits(int num) {
@@ -19,6 +23,16 @@ public class CountingBits {
     }
 
     //O(n)
+
+    /**
+     * In decimal, when you multiply by 10 a zero is added.
+     * Similarly if you multiply by 2 to a binary number, a zero is added and and
+     * odd number ends with 1.
+     * So when a even number 'n' is encountered, it will have same number of 1's as n/2
+     * And an odd number will have bits in n/2 +1
+     * @param num
+     * @return
+     */
     public static int[] countBits1(int num) {
         int arr[] = new int[num+1];
         arr[0]=0;
@@ -26,8 +40,9 @@ public class CountingBits {
             arr[i] = i%2==0 ? arr[i/2] : arr[i/2]+1;
         return arr;
     }
+
     public static void main(String[] args) {
-        int [] result = countBits1( 2 );
+        int [] result = countBits( 11 );
 
         for( int num: result ){
             System.out.println(num);
