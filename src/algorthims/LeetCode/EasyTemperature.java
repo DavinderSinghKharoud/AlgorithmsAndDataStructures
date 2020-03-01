e9770e1caa779ba31681cfce7e7896e9ab85e75a
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-01 00:31:19 -0500

    EasyTemperature(Using Stack Version 2)

--- src/algorthims/LeetCode/EasyTemperature.java
+++ src/algorthims/LeetCode/EasyTemperature.java
@@ -69,9 +69,20 @@ public class EasyTemperature {
         return result;
     }
 
+    //Using Stack 2
+    public static int[] dailyTemperatures2(int[] T) {
+        int[] ans = new int[T.length];
+        Stack<Integer> stack = new Stack();
+        for (int i = T.length - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && T[i] >= T[stack.peek()]) stack.pop();
+            ans[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
 
-        int[] result = dailyTemperatures1(new int[]{
+        int[] result = dailyTemperatures2(new int[]{
                 73, 74, 75, 71, 69, 72, 76, 73
         });
 
