be09d8177c2a5e629adfd7e77060d42a4edd848d
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-04 23:58:43 -0500

    Reverse String

--- src/algorthims/LeetCode/ReverseString.java
+++ src/algorthims/LeetCode/ReverseString.java
@@ -4,13 +4,7 @@ public class ReverseString {
 
     public static void reverseString(char[] s) {
 
-        for( int i = 0; i < s.length/2 ; i++ ){
 
-            char temp = s[i];
-            s[ i ] = s[ (s.length - 1) - i ];
-            s[ s.length - 1 - i] = temp;
-
-        }
 
         System.out.println( s );
 
