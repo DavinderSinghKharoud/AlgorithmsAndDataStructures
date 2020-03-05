95324a3ad609b38fa91952c4576d51a03c0adf1c
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-04 23:30:14 -0500

    House Robber 3

--- src/algorthims/LeetCode/HouseRobber.java
+++ src/algorthims/LeetCode/HouseRobber.java
@@ -51,7 +51,7 @@ public class HouseRobber {
     public static void main(String[] args) {
 
         System.out.println( rob(new int[]{
-                1,1,1
+                1,2,3,1
         }));
     }
 }
