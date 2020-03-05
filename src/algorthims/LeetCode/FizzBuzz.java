413f9a5bf860aa17e0136120288c60935a8c8a93
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-05 00:02:18 -0500

    FizzBuzz( More Faster )

--- src/algorthims/LeetCode/FizzBuzz.java
+++ src/algorthims/LeetCode/FizzBuzz.java
@@ -30,41 +30,19 @@ public class FizzBuzz {
 
         public List<String> fizzBuzz2(int n) {
 
-            // ans list
-            List<String> ans = new ArrayList<String>();
-
-            // Hash map to store all fizzbuzz mappings.
-            HashMap<Integer, String> fizzBizzDict =
-                    new HashMap<Integer, String>() {
-                        {
-                            put(3, "Fizz");
-                            put(5, "Buzz");
-                        }
-                    };
-
-            for (int num = 1; num <= n; num++) {
-
-                String numAnsStr = "";
-
-                for (Integer key : fizzBizzDict.keySet()) {
-
-                    // If the num is divisible by key,
-                    // then add the corresponding string mapping to current numAnsStr
-                    if (num % key == 0) {
-                        numAnsStr += fizzBizzDict.get(key);
-                    }
-                }
-
-                if (numAnsStr.equals("")) {
-                    // Not divisible by 3 or 5, add the number
-                    numAnsStr += Integer.toString(num);
-                }
-
-                // Append the current answer str to the ans list
-                ans.add(numAnsStr);
+            List<String>s=new ArrayList<String>();
+            for(int i=1;i<=n;i++)
+            {
+                if(i%3==0 && i%5!=0)
+                    s.add("Fizz");
+                else if(i%5==0 && i%3!=0)
+                    s.add("Buzz");
+                else if(i%3==0 && i%5==0)
+                    s.add("FizzBuzz");
+                else
+                    s.add(Integer.toString(i));
             }
-
-            return ans;
+            return s;
         }
 
 
