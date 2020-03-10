06057b4231cb14222b067b477d0e8194b831474f
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-09 20:25:09 -0400

    Number of 1 Bits( Another approach )

--- src/algorthims/LeetCode/Numberof1Bits.java
+++ src/algorthims/LeetCode/Numberof1Bits.java
@@ -26,7 +26,18 @@ public class Numberof1Bits {
         return bits;
     }
 
-    public static void main(String[] args) {
+    public static int hammingWeight1(int n) {
+
+        int sum = 0;
+
+        while ( n != 0 ){
+            sum++;
+            n = n & n-1;
+        }
+        return sum;
+    }
+
+        public static void main(String[] args) {
 
         System.out.println( hammingWeight( -3) );
     }
