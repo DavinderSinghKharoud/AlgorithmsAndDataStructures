ddc565a8b037cc399b3e6cb192704f6242df9c31
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-11 20:57:08 -0400

    Longest Common Prefix( Other Methods )

--- src/algorthims/LeetCode/LongestCommonPrefix.java
+++ src/algorthims/LeetCode/LongestCommonPrefix.java
@@ -2,7 +2,8 @@ package algorthims.LeetCode;
 
 public class LongestCommonPrefix {
 
-    public static String longestCommonPrefix(String[] strs) {
+    //Brute Force
+    public static String longestCommonPrefix1(String[] strs) {
 
         if (strs.length == 0) {
             return "";
@@ -38,9 +39,48 @@ public class LongestCommonPrefix {
         return stringBuilder.toString();
     }
 
+    //Horizontal scanning
+    public static String longestCommonPrefix2(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+
+    //Binary Search
+    public static String longestCommonPrefix3(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs)
+            minLen = Math.min(minLen, str.length());
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (isCommonPrefix(strs, middle))
+                low = middle + 1;
+            else
+                high = middle - 1;
+        }
+        return strs[0].substring(0, (low + high) / 2);
+    }
+
+    private static boolean isCommonPrefix(String[] strs, int len){
+        String str1 = strs[0].substring(0,len);
+        for (int i = 1; i < strs.length; i++)
+            if (!strs[i].startsWith(str1))
+                return false;
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(longestCommonPrefix(new String[]{
+        System.out.println(longestCommonPrefix3(new String[]{
                 "flower", "flow", "flight"
         }));
     }
