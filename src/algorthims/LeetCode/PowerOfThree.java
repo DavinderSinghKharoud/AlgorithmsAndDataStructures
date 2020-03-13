28f83582896852b80393aeaf946913c538409e6d
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-12 21:34:06 -0400

    Power of Three.

---
+++ src/algorthims/LeetCode/PowerOfThree.java
@@ -0,0 +1,18 @@
+package algorthims.LeetCode;
+
+public class PowerOfThree {
+    public static boolean isPowerOfThree(int n) {
+        if( n == 0){
+            return false;
+        }
+        while( n%3 == 0 ){
+            n = n/3;
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( isPowerOfThree(45));
+    }
+}
