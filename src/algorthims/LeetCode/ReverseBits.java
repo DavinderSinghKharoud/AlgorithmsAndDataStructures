996ea6a0fbcc4d8658d7e8eda54a3b3e9ced9cae
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-13 00:04:45 -0400

    Valid Palindrome

--- src/algorthims/LeetCode/ReverseBits.java
+++ src/algorthims/LeetCode/ReverseBits.java
@@ -24,7 +24,17 @@ public class ReverseBits {
 
     public static void main(String[] args) {
 
-        System.out.println( reverseBits(43261596));
+        System.out.println( reverseBits1(43261596));
+    }
+
+    public static int reverseBits1(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int rightbit = n & 1;
+            n = n >> 1;
+            res = i < 31 ?  (res | rightbit) << 1 : (res | rightbit);
+        }
+        return res;
     }
 
 }
