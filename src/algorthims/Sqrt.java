39cbcad47ab1b539b2c0a0e6be918358a3302ddd
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-13 23:09:09 -0400

    Sqrt

---
+++ src/algorthims/Sqrt.java
@@ -0,0 +1,12 @@
+package algorthims;
+
+public class Sqrt {
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println( mySqrt(4));
+    }
+}
