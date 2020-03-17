edb371cb3c3628b930cb68575c592cd28c75fb09
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-16 23:35:57 -0400

    Rotate Image

---
+++ src/algorthims/LeetCode/RotateImage.java
@@ -0,0 +1,43 @@
+package algorthims.LeetCode;
+
+public class RotateImage {
+
+    public static void rotate(int[][] matrix) {
+
+        //Do the transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //Then just flip the matrix horizontally( two sides approach and meet in the middle )
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        rotate( mat );
+
+        for( int i = 0; i<mat.length; i++ ){
+            for( int j = 0; j<mat[0].length; j++){
+                System.out.print(mat[i][j]+", ");
+            }
+            System.out.println();
+        }
+
+    }
+}
