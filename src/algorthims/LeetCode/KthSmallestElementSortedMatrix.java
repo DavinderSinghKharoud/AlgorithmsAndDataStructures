8c96287d91b5419ac5316251104bb64f26c826fc
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-18 20:43:03 -0400

    kth Smallest Elemen in Sorted Matrix( Binary Search Approach )

--- src/algorthims/LeetCode/KthSmallestElementSortedMatrix.java
+++ src/algorthims/LeetCode/KthSmallestElementSortedMatrix.java
@@ -35,8 +35,28 @@ public class KthSmallestElementSortedMatrix {
         return maxHeap.remove();
     }
 
+    public static int kthSmallest3(int[][] nums, int k) {
+        int N = nums.length;
+        int low = nums[0][0], high = nums[N - 1][N - 1];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0, j = N - 1;
+            for (int i = 0; i < N; ++i) {
+                while (j >= 0 && nums[i][j] > mid) {
+                    j--;
+                }
+                count += (j + 1);
+            }
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
     public static void main(String[] args) {
-        System.out.println(kthSmallest2(new int[][]{
+        System.out.println(kthSmallest3(new int[][]{
                 {1, 5, 9},
                 {10, 11, 13},
                 {12, 13, 15}
