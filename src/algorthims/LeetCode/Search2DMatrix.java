56e7ae2653a84cd43bf79a75f68d3037c8b63323
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-03-28 00:46:25 -0400

    Search 2D Matrix

--- src/algorthims/LeetCode/Search2DMatrix.java
+++ src/algorthims/LeetCode/Search2DMatrix.java
@@ -1,4 +1,5 @@
-	
+//package algorthims.LeetCode;
+
 /**
  * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
@@ -16,7 +17,9 @@ Consider the following matrix:
   [18, 21, 23, 26, 30]
 ]**/
 class Search2DMatrix {
-    public static boolean searchMatrix(int[][] matrix, int target) {
+	
+	//O(mn)
+    private static boolean searchMatrix(int[][] matrix, int target) {
         
         for( int i = 0; i<matrix.length; i++ ){
 			for( int j= 0; j< matrix[0].length; j++ ){
@@ -29,11 +32,36 @@ class Search2DMatrix {
 		return false;
     }
     
+    //O( m + n )
+    private static boolean searchMatrix2(int[][] matrix, int target) {
+             
+		if( matrix.length == 0){
+            return false;
+        }
+		
+		return search( matrix, target, 0, matrix[0].length - 1);
+    }
     
+    private static boolean search( int[][] matrix, int target, int row, int col){
+		if( row < 0 || row >= matrix.length || col >= matrix[0].length || col < 0){
+			return false;
+		}
+		
+		if( matrix[row][col] == target){
+			return true;
+		}
+		
+		if( matrix[row][col] > target ){
+			return search( matrix, target, row, col - 1 );
+		}
+			
+		return search( matrix, target, row + 1, col );
+	}
+	
     public static void main(String[] args) {
 
 
-        System.out.println( searchMatrix( new int[][]{
+        System.out.println( searchMatrix2( new int[][]{
 			{1,   4,  7, 11, 15},
 			{2,   5,  8, 12, 19},
 			{10, 13, 14, 17, 24},
