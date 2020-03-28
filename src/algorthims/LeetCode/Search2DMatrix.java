2f1223f1981c5cdaf82872d10962be65a731a973
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-03-28 00:02:09 -0400

    Search2DMatrix

--- src/algorthims/LeetCode/Search2DMatrix.java
+++ src/algorthims/LeetCode/Search2DMatrix.java
@@ -1,12 +1,43 @@
+	
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]**/
 class Search2DMatrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        return true;
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        
+        for( int i = 0; i<matrix.length; i++ ){
+			for( int j= 0; j< matrix[0].length; j++ ){
+				if( matrix[i][j] == target){
+					return true;
+				}
+			}
+		}
+		
+		return false;
     }
     
     
     public static void main(String[] args) {
 
-        System.out.println( "Hello");
+
+        System.out.println( searchMatrix( new int[][]{
+			{1,   4,  7, 11, 15},
+			{2,   5,  8, 12, 19},
+			{10, 13, 14, 17, 24},
+			{18, 21, 23, 26, 30}
+		}, 12));
 	}
 }
