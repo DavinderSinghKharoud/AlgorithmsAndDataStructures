226d2f7f7cc2538e260d80ef44ee270b8ab851aa
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-03-27 22:53:28 -0400

    Peak Element

--- src/algorthims/LeetCode/PeakElement.java
+++ src/algorthims/LeetCode/PeakElement.java
@@ -1,22 +1,59 @@
 
+/*
+ * A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+* */
+
 public class PeakElement {
-	    public static int findPeakElement(int[] nums) {
+	
+	//O(n)
+	    private static int findPeakElement1(int[] nums) {
 				  
-		  if( nums.length == 1 ) return 0;
-		  int num  = nums[0];
-		  for( int i = 1; i<nums.length; i++ ){
-			if( num > nums[ i ]){
-			      return i - 1;
-			}
-			if( i == nums.length - 1){return nums.length - 1;}
-			num = nums[ i ];
-			
+		for (int i = 0; i < nums.length - 1; i++) {
+		  if (nums[i] > nums[i + 1])
+                return i;
 		  }
-		  return -1;
+        return nums.length - 1;
 	    }
+	    
+	    //O( log(n) );
+	    private static int findPeakElement2(int[] nums) {
+			
+			return search( nums, 0, nums.length - 1 );
+	    }
+	    
+	    private static int search( int[] nums, int start, int end){
+			
+			if( start == end ){
+				return start;
+			}
+			
+			int mid = ( start + end)/2;
+			int num = nums[ mid ];
+			
+			if(num < nums[ mid + 1 ] ){
+				return search( nums, mid + 1, end);
+			}
+				
+			return search( nums, start, mid );
+			
+			
+		}
+		
 	    public static void main(String[] args) {
-			 System.out.println( findPeakElement( new int[] {
-			       1,2,3,1}) );
+			 System.out.println( findPeakElement2( new int[] {
+			       1,2,3,2}) );
 		}
       
       }
---
+++ src/algorthims/LeetCode/Search2DMatrix.java
@@ -0,0 +1,12 @@
+
+class Search2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return true;
+    }
+    
+    
+    public static void main(String[] args) {
+
+        System.out.println( "Hello");
+	}
+}
