c3b3952128c6d6d8dad1a2b0389b812c551726cb
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-04-08 22:20:01 -0400

    Jump Game

--- src/algorthims/LeetCode/JumpGame.java
+++ src/algorthims/LeetCode/JumpGame.java
@@ -1,28 +1,34 @@
-package algorthims.LeetCode;
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
+Each element in the array represents your maximum jump length at that position.
 
-//Not Done yet
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ */
 public class JumpGame {
 
     public static void main(String[] args) {
         System.out.println( checkReach( new int[]{
-                2,2,5,6
+               2,0
         }) );
     }
 
-    private static boolean checkReach(int[] arry) {
-        int index = 0;
-        int finalIndex = arry.length - 1;
-        while ( index != finalIndex && index<arry.length ){
+    private static boolean checkReach(int[] nums) {
+		
+	    int len = nums.length;
+	    int lastPos = len - 1;
+		for( int i = len - 1; i>=0; i-- ){
+			  if( i + nums[i] >= lastPos){
+			  	lastPos = i;
+			  }
+		}
 
-            int value = arry[index];
-            if( value == 0){
-                return false;
-            }
-            index+=value;
-
-        }
-
-        return true;
-    }
+			return lastPos == 0;
+      }
 }
