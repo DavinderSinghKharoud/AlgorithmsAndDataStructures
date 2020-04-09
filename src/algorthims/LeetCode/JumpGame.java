0068d51bc1fb4be4b69fdeb5fad31605d1184d04
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-04-08 22:27:40 -0400

    Jump Game( Dynamic Programming )

--- src/algorthims/LeetCode/JumpGame.java
+++ src/algorthims/LeetCode/JumpGame.java
@@ -14,11 +14,12 @@ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 public class JumpGame {
 
     public static void main(String[] args) {
-        System.out.println( checkReach( new int[]{
+        System.out.println( canJump( new int[]{
                2,0
         }) );
     }
 
+    //Greedy More Efficient
     private static boolean checkReach(int[] nums) {
 		
 	    int len = nums.length;
@@ -31,4 +32,24 @@ public class JumpGame {
 
 			return lastPos == 0;
       }
+
+      //Dynamic Programming
+      public static boolean canJump(int[] nums) {
+		     int dp[] = new int[nums.length];
+
+		     dp[nums.length - 1] = 1;
+
+		  for (int i = nums.length - 1; i >= 0; i--) {
+			      int reach = Math.min( i + nums[i], nums.length - 1);
+
+			  for (int j = i + 1; j <= reach ; j++) {
+				   if( dp[j] == 1 ){
+				   	dp[i] = 1;
+				   	break;
+				   }
+			  }
+		  }
+
+		  return dp[0] == 1;
+	  }
 }
