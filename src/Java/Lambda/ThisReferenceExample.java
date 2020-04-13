6c30d9824e3841ebfc99a71439270d2f36482f4c
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-04-13 00:26:35 -0400

    Reference Example with lambda

---
+++ src/Java/Lambda/ThisReferenceExample.java
@@ -0,0 +1,34 @@
+package Java.Lambda;
+
+public class ThisReferenceExample {
+
+    public void doProcess(int i, Process p){
+        p.process(i);
+    }
+
+    public void execute(){
+        //this == ?
+        doProcess(10, i -> {
+            System.out.println("Value of i is " + i);
+            System.out.println(this);
+        });
+
+    }
+
+    public String toString(){
+        return "This is the main ThisReferenceExample class instance";
+    }
+    public static void main(String[] args) {
+        ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
+
+//        thisReferenceExample.doProcess(10, i -> {
+//            System.out.println("Value of i is " + i );
+//
+//            //System.out.println(this); This will not work
+//
+//        });
+
+        thisReferenceExample.execute();
+    }
+}
+
