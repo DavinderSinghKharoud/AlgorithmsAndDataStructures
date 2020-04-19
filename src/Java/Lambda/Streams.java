9bfcbed51381a0bdc67403fa9669789e72c2e356
Davinder Singh Kharoud <dskharoud@gmail.com>
2020-04-18 21:29:52 -0400

    Streams Basic

---
+++ src/Java/Lambda/Streams.java
@@ -0,0 +1,22 @@
+package Java.Lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Streams {
+
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 40),
+                new Person("Lewis", "Carrol", 40),
+                new Person("Thomas", "Carlyle", 50),
+                new Person("Charlotte", "Bronte", 50),
+                new Person("Matthew", "Arnold", 60)
+
+        );
+
+        people.stream()
+                .filter( person -> person.getLastName().startsWith("C"))
+                .forEach(System.out::println);
+    }
+}
