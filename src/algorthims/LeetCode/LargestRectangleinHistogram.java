f43a4586a8af678681d54a65eea9ca75a58deeba
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-05-10 15:18:03 -0400

    Largest Rectangular in Histogram ( Stack )

--- src/algorthims/LeetCode/LargestRectangleinHistogram.java
+++ src/algorthims/LeetCode/LargestRectangleinHistogram.java
@@ -3,10 +3,12 @@ package algorthims.LeetCode;
 /**
  * Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.**/
  
+ import java.util.*;
 
 public class LargestRectangleinHistogram {
-	
-	public static int largestRectangleArea(int[] heights) {
+
+	//O(n square) time complexity
+	public static int largestRectangleArea1(int[] heights) {
 		
 		int max = 0;
 		int len = heights.length;
@@ -43,9 +45,32 @@ public class LargestRectangleinHistogram {
     
 	public static void main (String[] args) {
 		
-		System.out.println( largestRectangleArea( new int[]{
+		System.out.println( largestRectangleArea2( new int[]{
 			2,1,5,6,2,3
 		}));
+	}
+
+	//using stack
+	public static int largestRectangleArea2(int[] heights) {
+
+			Stack<Integer> stack = new Stack<>();
+			int max = 0;
+			
+			
+			for( int index = 0; index<=heights.length; index++ ){
+
+				while ( !stack.isEmpty() && heights[ stack.peek() ] > ((index == heights.length) ? -1: heights[index]) ){
+					int val = stack.pop();
+					int range = (stack.isEmpty()) ? index: index - 1 - stack.peek();
+					max = Math.max( max, range * heights[val] );
+				}
+
+				stack.push( index );
+			}
+			
+
+			return max;
+		
 	}
 }
 
