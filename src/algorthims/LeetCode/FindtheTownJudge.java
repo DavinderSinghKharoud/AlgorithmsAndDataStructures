c01132ad40052c3a082d10dc46388067b40b734b
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-05-10 11:40:23 -0400

    Find the Town Judge

---
+++ src/algorthims/LeetCode/FindtheTownJudge.java
@@ -0,0 +1,77 @@
+package algorthims.LeetCode;
+/**
+ * In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+ 
+
+Example 1:
+
+Input: N = 2, trust = [[1,2]]
+Output: 2
+* **/
+
+import java.util.*;
+
+public class FindtheTownJudge {
+
+	//O(n len ) time complexity
+	public static int findJudge(int N, int[][] trust) {
+        
+        int max;
+        boolean check;
+
+		for (int i = 1; i <= N; i++) {
+			max = 0;
+			check = true;
+			for( int[] pair: trust ){
+				if( pair[0] == i ) {
+					check = false;
+					break;
+				}
+				if( pair[1] == i ){
+					max++;
+				}
+			}
+
+			if( check && max == N - 1 ){
+				return i;
+			}
+		}
+		
+		return -1;
+    }
+    
+	public static void main (String[] args) {
+		
+//		System.out.println( findJudge( 4, new int[][]{
+//			{1, 3}, {1, 4}, {2, 3},{2, 4}, {4, 3}
+//		}));
+		System.out.println( findJudge( 3, new int[][]{
+				{1, 3}, {2, 3},{3, 1}
+		}));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
