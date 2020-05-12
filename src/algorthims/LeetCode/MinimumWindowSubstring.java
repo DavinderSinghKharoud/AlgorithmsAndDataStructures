0b859dcc6bcd7cae2d2e8d207a08daeac1f29cbc
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-05-11 23:05:47 -0400

    MinimumWindowSubstring

--- src/algorthims/LeetCode/MinimumWindowSubstring.java
+++ src/algorthims/LeetCode/MinimumWindowSubstring.java
@@ -72,7 +72,102 @@ public class MinimumWindowSubstring {
     
 	public static void main (String[] args) {
 		
-		System.out.println( minWindow( "a", "aa"));
+		System.out.println( minWindow3( "ADOBECODEBANC", "ABC"));
 	}
+
+
+	//Sliding window approach
+	//O(n) time complexity
+	public static String minWindow2(String s, String t) {
+		
+		if( t.length() == 0 ){
+			return "";
+		}
+		
+		//for t
+		int[] freq1 = new int[128];
+		//for s
+		int[] freq2 = new int[128];
+		
+		//adding all the characters of t to array
+		t.chars().forEach( c -> freq1[ c ] ++ );
+		
+		int start = -1, end = -1;
+		
+		//
+		for( int left = 0, right = 0; right < s.length(); right++ ){
+			
+			freq2[ s.charAt(right) ]++;
+			
+			//if it contain all the elements
+			while( isOk( freq1, freq2 ) ){
+				if( start == -1 || end - start > right - left ){
+					start = left;
+					end = right;
+				}
+				
+				//remove the left character and increase the left boundary
+				freq2[s.charAt(left) ]--;
+				left++;
+			}
+			
+			
+		}
+		
+		return start == -1 ? "": s.substring( start, end + 1);
+	}
+	
+	private static boolean isOk( int[] freq1, int[] freq2 ){
+		
+		//verify if all the character of t are in s string
+		for( int i = 0; i<128; i++ ){
+			if( freq1[i] > freq2[i] ){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	//Sliding window approach
+	//more efficient
+	public static String minWindow3(String s, String t) {
+		char[] ca1 = s.toCharArray(), ca2 = t.toCharArray();
+		if (ca2.length > ca1.length) {
+			return "";
+		}
+		int[] m = new int[128];
+		for (char c : ca2) {
+			m[c]++;
+		}
+		int left = 0, right = Integer.MAX_VALUE;
+		for (int l = 0, r = 0, curr = ca2.length; r < ca1.length; r++) {
+			if (m[ca1[r]]-- > 0) {
+				curr--;
+			}
+			while (curr == 0) {
+				if (right - left > r - l) {
+					left = l;
+					right = r;
+				}
+				if (++m[ca1[l++]] > 0) {
+					curr++;
+				}
+			}
+		}
+		return right == Integer.MAX_VALUE ? "" : s.substring(left, right + 1);
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
