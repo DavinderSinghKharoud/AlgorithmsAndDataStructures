5670509c322187bd883ab7752bcbd5890b2fdf65
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-05-14 00:36:41 -0400

    Word Search II( Hard ) Efficient Solution using Trie data structure

--- src/algorthims/LeetCode/WordSearchII.java
+++ src/algorthims/LeetCode/WordSearchII.java
@@ -8,6 +8,7 @@ import java.util.*;
  */
 public class WordSearchII {
 
+    //O(board.length) space complexity and time complexity O(not sure 4 is to power m), as we need to traverse the whole 2d array for each word
     public static List<String> findWords(char[][] board, String[] words) {
 
         HashMap<Character, List<int[]>> map = new HashMap<>();
@@ -75,13 +76,94 @@ public class WordSearchII {
         String[] list = new String[]{"oath", "pea", "eat", "rain"};
 
 
-        //System.out.println(findWords(board, list));
+        System.out.println(findWords(board, list));
 
-        System.out.println( findWords( new char[][]{
-                {'a','a'}
-        }, new String[]{
-                "a"
-        }));
+//        System.out.println( findWords2( new char[][]{
+//                {'a','a'}
+//        }, new String[]{
+//                "a"
+//        }));
+    }
+
+    //O(m * all the board character) time complexity and O(totol length of all the characters in the words ) space complexity
+    public static List<String> findWords2(char[][] board, String[] words) {
+
+		Trie trie = new Trie();
+		for( String word: words ){
+			trie.insert( word );
+		}
+		
+		Set<String> resSet = new HashSet<>();	
+		
+		for( int i = 0; i<board.length; i++ ){
+			for( int j = 0; j<board[0].length; j++ ){
+				explore( board, i, j, resSet, trie.root);
+			}
+		}
+		
+		return new ArrayList<>(resSet);
+    }
+    
+    public static void explore( char[][] board, int row, int col, Set<String> resSet, TrieNode root){
+		
+		if( row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] == '#' ){
+			return;
+		}
+		
+		if( !root.children.containsKey( board[row][col] )){
+			return;
+		}
+		
+		char temp = board[row][col];
+
+		root = root.children.get( board[row][col] );
+        //just to make sure we do not again visit this node
+        board[row][col] = '#';
+		//if we reach the end of trie
+		if( root.children.containsKey( '*' ) ){
+			resSet.add( root.word );
+		}
+		
+		//explore all paths 
+		explore( board, row - 1, col, resSet, root );
+		explore( board, row + 1, col, resSet, root );
+		explore( board, row , col - 1, resSet, root );
+		explore( board, row , col + 1, resSet, root );
+		
+		board[row][col] = temp;
+	}
+
+    static class TrieNode{
+        private HashMap<Character, TrieNode> children = new HashMap<>();
+        String word;
+    }
+
+    static class Trie{
+        private TrieNode root;
+        char end ='*';
+
+        public Trie(){
+            root = new TrieNode();
+        }
+
+        //Inserts a word
+        public void insert( String word ){
+			
+			TrieNode node = root;
+            for( int index = 0; index<word.length(); index ++ ){
+				
+				char c = word.charAt(index);
+				if( !node.children.containsKey(c) ){
+					TrieNode trieNode = new TrieNode();
+					node.children.put(c, trieNode );
+				}
+				node = node.children.get(c);
+			}
+			
+			node.children.put(end, null);
+			//so that we do not need to add the characters to get the final string
+			node.word = word;
+        }
     }
 }
 
