26936fb7943341efab9fff2f85889e57f9213a66
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-05-23 14:22:26 -0400

    Longest valid parentheses( more efficient )

--- src/algorthims/LeetCode/LongestValidParentheses.java
+++ src/algorthims/LeetCode/LongestValidParentheses.java
@@ -17,8 +17,7 @@ import java.util.*;
 public class LongestValidParentheses {
 	
 	//O(n) time complexity and space complexity
-	public static int longestValidParentheses(String s) {
-       // if( s.length() == 0 ) return 0;
+	public static int longestValidParentheses1(String s) {
         int max_len = 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
@@ -40,7 +39,52 @@ public class LongestValidParentheses {
 		return max_len;
     }
 	public static void main (String[] args) {
-		System.out.println( longestValidParentheses(""));
+		System.out.println( longestValidParentheses2("(())"));
+	}
+	
+	//O(n) time complexity and O(1) space complexity
+	public static int longestValidParentheses2(String s) {
+		int left = 0, right = 0;
+		int max = 0;
+		//left scan
+		for( int i = 0; i<s.length(); i++ ){
+			char c = s.charAt(i);
+			if( c == '(' ){
+				left++;
+			}else{
+				right++;
+			}
+			
+			if( left == right ){
+				max = Math.max( max, right * 2 );
+			}
+			
+			if( right > left ){
+				left = right = 0;
+			}
+		}
+		
+		left = right = 0;
+		//right scan 
+		for( int index = s.length() - 1; index >= 0; index-- ){
+			char c = s.charAt(index);
+			
+			if( c == '(' ){
+				left++;
+			}else{
+				right++;
+			}
+			
+			if( left == right ){
+				max = Math.max( max, right * 2 );
+			}
+			
+			if( left > right ){
+				left = right = 0;
+			}
+		}
+		
+		return max;
 	}
 }
 
