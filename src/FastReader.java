e50eae4aa4e9b3d0a1f9c9c622a2f6593d8a04e9
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-05-24 22:09:46 -0400

    Unique Paths 2

--- src/FastReader.java
+++ src/FastReader.java
@@ -1,63 +1,63 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.StringTokenizer;
 
 
-     class FastReader
-    {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader()
+         class FastReader
         {
-            br = new BufferedReader(new
-                    InputStreamReader(System.in));
-        }
+            BufferedReader br;
+            StringTokenizer st;
 
-        String next()
-        {
-            while (st == null || !st.hasMoreElements())
+            public FastReader()
             {
+                br = new BufferedReader(new
+                        InputStreamReader(System.in));
+            }
+
+            String next()
+            {
+                while (st == null || !st.hasMoreElements())
+                {
+                    try
+                    {
+                        st = new StringTokenizer(br.readLine());
+                    }
+                    catch (IOException  e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                return st.nextToken();
+            }
+
+            int nextInt()
+            {
+                return Integer.parseInt(next());
+            }
+
+            long nextLong()
+            {
+                return Long.parseLong(next());
+            }
+
+            double nextDouble()
+            {
+                return Double.parseDouble(next());
+            }
+
+            String nextLine()
+            {
+                String str = "";
                 try
                 {
-                    st = new StringTokenizer(br.readLine());
+                    str = br.readLine();
                 }
-                catch (IOException  e)
+                catch (IOException e)
                 {
                     e.printStackTrace();
                 }
+                return str;
             }
-            return st.nextToken();
         }
-
-        int nextInt()
-        {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong()
-        {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble()
-        {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine()
-        {
-            String str = "";
-            try
-            {
-                str = br.readLine();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
--- src/algorthims/LeetCode/RotateImage.java
+++ src/algorthims/LeetCode/RotateImage.java
@@ -28,6 +28,8 @@ public class RotateImage {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
+//                {1, 2},
+//                {3, 4}
         };
 
         rotate( mat );
