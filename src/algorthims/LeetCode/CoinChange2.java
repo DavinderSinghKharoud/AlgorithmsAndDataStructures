edded488492521c3833d45e8aff07ef47fc4eb6f
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-06-07 23:08:36 -0400

    Coin Change 2 ( More efficient )

--- src/algorthims/LeetCode/CoinChange2.java
+++ src/algorthims/LeetCode/CoinChange2.java
@@ -52,5 +52,18 @@ public class CoinChange2 {
         return dp[coins.length][amount];
     }
 
+    //O( n * amount ) time complexity and O( amount ) space complexity
+    public static int change3(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
 
+        dp[0] = 1;
+
+        for( int coin: coins ){
+            for( int value = coin; value < amount + 1; value++ ){
+                dp[value] += dp[ value - coin ];
+            }
+        }
+
+        return dp[amount];
+    }
 }
