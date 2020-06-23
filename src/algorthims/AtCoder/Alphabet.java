ce82e2ed18a766f545535138f353aabb8f8cdf7a
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-06-22 23:32:54 -0400

    Alphabet

---
+++ src/algorthims/AtCoder/Alphabet.java
@@ -0,0 +1,14 @@
+package algorthims.AtCoder;
+
+import java.util.Scanner;
+
+ class Alphabet {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        String lower = input.toLowerCase();
+
+        System.out.println( ( lower.equals(input) ) ? 'a': 'A');
+    }
+}
