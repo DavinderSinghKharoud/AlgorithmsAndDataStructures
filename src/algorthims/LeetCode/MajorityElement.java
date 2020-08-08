574705a122bd110f27f78fb1d2f9fed0825fbbe4
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-08-08 16:45:27 -0400

    Majority Element

--- src/algorthims/LeetCode/MajorityElement.java
+++ src/algorthims/LeetCode/MajorityElement.java
@@ -2,6 +2,7 @@ package algorthims.LeetCode;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Given an array of size n, find the majority element.
@@ -14,13 +15,40 @@ public class MajorityElement {
 
     public static void main(String[] args) {
 
-        System.out.println( majorityElement(new int[]{
-                2,2,1,1,1,2,2
-        }));
+        /*We can solve in many ways
+        1.] brute force (  O(n square) time complexity )
+        2.] Using space ( O(n) space and time complexity )
+        3.] Using sorting ( O(nLog(n)) time complexity
+        4.] Boyer-Moore Voting Algorithm ( O(n) time and O(1) space complexity )
+        */
+        System.out.println( majorityElement2(Arrays.asList( 2,2,1,1,1,2,2)) );
 
     }
 
+    //O(n) time and O(1) space complexity
+    public static int majorityElement2(List<Integer> lst) {
 
+        int total = 0;
+        Integer candidate = null;
+        
+        for(int num: lst ){
+			
+			if( total == 0 ){
+				candidate = num;
+			}
+			
+			if( num == candidate ){
+				total++;
+			}else{
+				total--;
+			}
+			
+		}
+		
+		return candidate;
+    }
+
+    //O(N) time and space complexity
     public static int majorityElement(int[] nums) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -49,9 +77,5 @@ public class MajorityElement {
 
     }
 
-//    Sorting
-//    public static int majoElem(int[] nums){
-//        Arrays.sort(nums);
-//        return nums[nums.length/2];
-//    }
+
 }
