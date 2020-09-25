c5fbf182d935160ceef1fd4eb1f3e26de09728e3
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-09-24 23:39:20 -0400

    Flatten Binary Tree

--- src/algorthims/LeetCode/FlattenBinaryTreetoLinkedList.java
+++ src/algorthims/LeetCode/FlattenBinaryTreetoLinkedList.java
@@ -3,28 +3,28 @@ package algorthims.LeetCode;
 import java.util.*;
 
 /**
- *Given a binary tree, flatten it to a linked list in-place.
-
-For example, given the following tree:
-
-    1
-   / \
-  2   5
- / \   \
-3   4   6
-The flattened tree should look like:
-
-1
- \
-  2
-   \
-    3
-     \
-      4
-       \
-        5
-         \
-          6
+ * Given a binary tree, flatten it to a linked list in-place.
+ * <p>
+ * For example, given the following tree:
+ * <p>
+ * 1
+ * / \
+ * 2   5
+ * / \   \
+ * 3   4   6
+ * The flattened tree should look like:
+ * <p>
+ * 1
+ * \
+ * 2
+ * \
+ * 3
+ * \
+ * 4
+ * \
+ * 5
+ * \
+ * 6
  */
 public class FlattenBinaryTreetoLinkedList {
 
@@ -33,9 +33,9 @@ public class FlattenBinaryTreetoLinkedList {
         if (root == null) return;
 
         Stack<TreeNode> stack = new Stack<>();
-        stack.push( root );
+        stack.push(root);
 
-        while ( !stack.isEmpty() ) {
+        while (!stack.isEmpty()) {
             TreeNode curr = stack.pop();
 
             if (curr.right != null) {
@@ -63,7 +63,7 @@ public class FlattenBinaryTreetoLinkedList {
         root.left.right = new TreeNode(4);
         root.right.right = new TreeNode(6);
 
-        flatten2(root);
+        flatten3(root);
 
         while (root != null) {
             System.out.println(root.val);
@@ -71,34 +71,53 @@ public class FlattenBinaryTreetoLinkedList {
         }
     }
 
-    
- public static void flatten2(TreeNode root) {
-        if(root == null)
+
+    public static void flatten2(TreeNode root) {
+        if (root == null)
             return;
-        
+
         TreeNode left = root.left;
         flatten(left);
         TreeNode right = root.right;
         flatten(right);
         //After this we assume the L and R child are right aligned lists.
-        
+
         //If we didn't have a left child, root is already done.
-        if(left == null)
+        if (left == null) {
             return;
+        }
         //If not then make the L child the right,
         // and append the R child to the end of the L list.
-        else {
-            root.right = left;
-            root.left = null;
-            if(right != null) {
-                    while(left.right != null) {
-                    left = left.right;
-                }
-                left.right = right;
+
+        root.right = left;
+        root.left = null;
+        if (right != null) {
+            while (left.right != null) {
+                left = left.right;
             }
+            left.right = right;
         }
+
     }
-		
+
+    public static TreeNode flatten3(TreeNode root) {
+        if( root == null ) return null;
+
+        if( root.left == null && root.right == null ){
+            return root;
+        }
+
+        TreeNode leftTail = flatten3(root.left);
+        TreeNode rightTail = flatten3(root.right);
+
+        if( leftTail != null ){
+            leftTail.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+
+        return ( rightTail == null ) ? leftTail: rightTail;
+    }
     public static class TreeNode {
         int val;
         TreeNode left;
