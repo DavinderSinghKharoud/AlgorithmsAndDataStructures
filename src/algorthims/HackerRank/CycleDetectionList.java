64c0d010099d35151df42421ac3d2e4fcf47fac8
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-10-25 11:04:57 -0400

    Cycle Detection List

---
+++ src/algorthims/HackerRank/CycleDetectionList.java
@@ -0,0 +1,41 @@
+package algorthims.HackerRank;
+
+public class CycleDetectionList {
+
+    static boolean hasCycle(SinglyLinkedListNode head) {
+
+
+        if (head == null) return false;
+
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = (head.next != null) ? head.next.next : null;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedListNode lst = new SinglyLinkedListNode(1);
+        //lst.next = new SinglyLinkedListNode(2);
+        //lst.next.next = new SinglyLinkedListNode(3);
+        //lst.next.next.next = lst;
+
+        System.out.println( hasCycle(lst));
+    }
+
+    static class SinglyLinkedListNode {
+        int data;
+        SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+}
