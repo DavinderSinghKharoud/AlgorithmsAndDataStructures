2e6971893ce9962d0d72f2ff0449ff598e2badae
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-11-22 13:08:47 -0500

    Find The Running Medians Using AVL Trees

--- src/algorthims/HackerRank/FindTheRunningMedians.java
+++ src/algorthims/HackerRank/FindTheRunningMedians.java
@@ -16,8 +16,8 @@ import java.util.*;
  * The first line contains a single integer, , denoting the number of integers in the data stream.
  * Each line  of the  subsequent lines contains an integer, , to be added to your list.
  * Constraints
- *
- *
+ * <p>
+ * <p>
  * Output Format
  * After each new integer is added to the list, print the list's updated median on a new line as a single double-precision number scaled to  decimal place (i.e.,  format).
  * Sample Input
@@ -73,7 +73,172 @@ public class FindTheRunningMedians {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(runningMedian(new int[]{0, 0})));
+        //System.out.println(Arrays.toString(runningMedian(new int[]{0, 0})));
+
+        System.out.println(Arrays.toString(runningMedian2(new int[]{12, 4, 5, 3, 8, 7})));
+
+    }
+
+
+    //O(log n) time complexity using AVL Trees and O(n) space
+    static double[] runningMedian2(int[] a) {
+        int len = a.length;
+        double[] res = new double[len];
+
+        if (a.length == 0) return res;
+
+        Node root = null;
+
+        for (int index = 0; index < len; index++) {
+            root = insert(root, a[index]);
+
+            res[index] = findMedian(root);
+        }
+        return res;
+    }
+
+    private static double findMedian(Node root) {
+        int firstIndex = size / 2 + 1;
+        int k1 = 0;
+        int k2 = 0;
+        double median = 0;
+
+        k1 = findKthSmallestNum(root, firstIndex);
+        median = k1 / 1.0;
+        if (size % 2 == 0) {
+            k2 = findKthSmallestNum(root, firstIndex - 1);
+            median = (k1 + k2) / 2.0;
+        }
+        return median;
+    }
+
+    private static int findKthSmallestNum(Node node, int k) {
+        if (k >= node.lcount + 1 && k < node.lcount + 1 + node.freq) return node.val;
+
+        if (k <= node.lcount) {
+            return findKthSmallestNum(node.left, k);
+        } else {
+            return findKthSmallestNum(node.right, k - node.lcount - node.freq);
+        }
+
+
+    }
+
+
+    //Using AVL Tree
+
+    static int size = 0;
+
+    static class Node {
+        Node left;
+        Node right;
+        int val;
+        int height = 0;
+        int freq = 1; //duplicates are allowed in stream
+        int lcount = 0; // number of items in left subtree
+        int rcount = 0; // number of items in right subtree
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    static Node insert(Node node, int val) {
+
+        if (node == null) {
+            size++;
+            return new Node(val);
+        }
+
+        if (node.val < val) {
+            node.right = insert(node.right, val);
+            node.rcount++;
+        } else if (node.val > val) {
+            node.left = insert(node.left, val);
+            node.lcount++;
+        } else {
+            node.freq++;
+            size++;
+            return node;
+        }
+
+        //Balance the tree
+        int balance = getHeight(node.left) - getHeight(node.right);
+
+        if (balance > 1) {
+
+            if (getHeight(node.left.left) >= getHeight(node.left.right)) {
+                //L - L case
+                node = rightRotate(node);
+            } else {
+                //L - R case
+                node.left = leftRotate(node.left);
+                node = rightRotate(node);
+            }
+
+        } else if (balance < -1) {
+
+            if (getHeight(node.right.right) >= getHeight(node.right.left)) {
+                node = leftRotate(node);
+            } else {
+                node.right = rightRotate(node.right);
+                node = leftRotate(node);
+            }
+        }
+
+        node.height = setHeight(node);
+
+        return node;
+    }
+
+
+    static Node rightRotate(Node node) {
+
+        Node newNode = node.left;
+        node.left = newNode.right;
+        newNode.right = node;
+
+        //*************************************************************
+        //Update count
+        node.lcount = (node.left != null) ? node.left.lcount + node.left.rcount + node.left.freq : 0;
+        newNode.rcount = node.lcount + node.rcount + node.freq;
+        //*************************************************************
+
+        //Update heights
+        node.height = setHeight(node);
+        newNode.height = setHeight(newNode);
+
+        return newNode;
+    }
+
+    static Node leftRotate(Node node) {
+        Node newNode = node.right;
+        node.right = newNode.left;
+        newNode.left = node;
+
+
+        //*************************************************************
+        //Update count
+        node.rcount = (node.right != null) ? node.right.lcount + node.right.rcount + node.right.freq : 0;
+        newNode.lcount = node.lcount + node.rcount + node.freq;
+        //*************************************************************
+
+        //Update heights
+        node.height = setHeight(node);
+        newNode.height = setHeight(newNode);
+
+
+        return newNode;
+
+    }
+
+    static int getHeight(Node node) {
+        if (node == null) return -1;
+        return node.height;
+    }
+
+    static int setHeight(Node node) {
+        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
     }
 
 
