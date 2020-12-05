d7f2ac8ef79f2e357a18dad0a26a5d8d76403174
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-12-04 21:49:39 -0500

    And Xor Or

--- src/algorithms/HackerRank/AndXorOr.java
+++ src/algorithms/HackerRank/AndXorOr.java
@@ -2,6 +2,27 @@ package algorithms.HackerRank;
 
 import java.util.*;
 
+/**
+ * Given an array  of  distinct elements. Let  and  be the smallest and the next smallest element in the interval where .
+ * .
+ * where , are the bitwise operators ,  and  respectively.
+ * Your task is to find the maximum possible value of .
+ * Input Format
+ * First line contains integer .
+ * Second line contains  integers, representing elements of the array .
+ * Constraints
+ *
+ *
+ * Output Format
+ * Print the value of maximum possible value of .
+ * Sample Input
+ * 5
+ * 9 6 3 5 2
+ * Sample Output
+ * 15
+ * Explanation
+ * Consider the interval  the result will be maximum.
+ */
 public class AndXorOr {
 
     static int andXorOr(int[] arr) {
