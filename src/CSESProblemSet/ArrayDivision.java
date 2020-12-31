12bbc1d7fd234c3cd762a54f70d67f11c4173368
DavinderSinghKharoud <Dskharoud2@gmail.com>
2020-12-30 21:32:49 -0500

    Array Division

---
+++ src/CSESProblemSet/ArrayDivision.java
@@ -0,0 +1,208 @@
+
+import java.io.*;
+import java.util.*;
+
+public class ArrayDivision {
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Reader fastReader = new Reader();
+
+    static void solve() throws IOException {
+			
+		int tests = fastReader.nextInt();
+		int k = fastReader.nextInt();
+
+		int[] arr = new int[tests];
+		long low = 0;
+		long high = 0;
+		for(int i = 0; i < tests; i++ ){
+			arr[i] = fastReader.nextInt();
+			high += arr[i];
+        }
+
+		long ans = -1;
+		while( low <= high ){
+			long mid = (high - low)/2 + low;
+			
+			if( check(mid, arr, k) ){
+                ans = mid;
+				high = mid - 1;
+
+			}else{
+				low = mid + 1;
+			}
+		}
+		
+		print(ans);
+		
+   }
+   
+   static boolean check( long mid, int[] arr, int k ){
+	   int groups = 0;
+	   long sum = 0;
+
+       for (int j : arr) {
+           if (j > mid) return false;
+
+           if (sum + j > mid) {
+               sum = 0;
+               groups++;
+           }
+
+           sum += j;
+       }
+	   
+	   if( sum > 0 ) groups++;
+	   return groups <= k;
+   }
+
+
+    /************************************************************************************************************************************************/
+    public static void main(String[] args) throws IOException {
+
+        solve();
+        out.close();
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+
+        }
+
+        public String next() throws IOException {
+
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    static void shuffle(int[] aa, int n) {
+        Random rand = new Random();
+        for (int i = 1; i < n; i++) {
+            int j = rand.nextInt(i + 1);
+            int tmp = aa[i];
+            aa[i] = aa[j];
+            aa[j] = tmp;
+        }
+    }
+
+    static void shuffle(int[][] aa, int n) {
+        Random rand = new Random();
+        for (int i = 1; i < n; i++) {
+            int j = rand.nextInt(i + 1);
+            int first = aa[i][0];
+            int second = aa[i][1];
+            aa[i][0] = aa[j][0];
+            aa[i][1] = aa[j][1];
+            aa[j][0] = first;
+            aa[j][1] = second;
+        }
+    }
+
+    static void print(Object object) {
+        out.print(object);
+    }
+
+    static void println(Object object) {
+        out.println(object);
+    }
+}
