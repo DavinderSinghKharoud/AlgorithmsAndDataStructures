9a8a1bb0a6bfff6e7eb1567645898bde7b67e762
DavinderSinghKharoud <Dskharoud2@gmail.com>
2021-01-03 21:55:53 -0500

    Removal Game

---
+++ src/CSESProblemSet/RemovalGame.java
@@ -0,0 +1,334 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * There is a list of n
+ * n
+ * numbers and two players who move alternately. On each move, a player removes either the first or last number from the list, and their score increases by that number. Both players try to maximize their scores.
+ * <p>
+ * What is the maximum possible score for the first player when both players play optimally?
+ * <p>
+ * Input
+ * <p>
+ * The first input line contains an integer n
+ * n
+ * : the size of the list.
+ * <p>
+ * The next line has n
+ * n
+ * integers x1,x2,…,xn
+ * x
+ * 1
+ * ,
+ * x
+ * 2
+ * ,
+ * …
+ * ,
+ * x
+ * n
+ * : the contents of the list.
+ * <p>
+ * Output
+ * <p>
+ * Print the maximum possible score for the first player.
+ * <p>
+ * Constraints
+ * 1≤n≤5000
+ * 1
+ * ≤
+ * n
+ * ≤
+ * 5000
+ * <p>
+ * −109≤xi≤109
+ * −
+ * 10
+ * 9
+ * ≤
+ * x
+ * i
+ * ≤
+ * 10
+ * 9
+ * <p>
+ * Example
+ * <p>
+ * Input:
+ * 4
+ * 4 5 1 3
+ * <p>
+ * Output:
+ * 8
+ */
+public class RemovalGame {
+    static PrintWriter out = new PrintWriter(System.out);
+    static Reader fastReader = new Reader();
+    static StringBuilder sbr = new StringBuilder();
+    static int mod = (int) 1e9 + 7;
+    static int dmax = Integer.MAX_VALUE;
+    static int dmin = Integer.MIN_VALUE;
+
+    //Time Limit Exceeded
+    static void solve() throws IOException {
+
+        int count = fastReader.intNext();
+        long[] arr = new long[count];
+
+        for (int i = 0; i < count; i++) {
+            arr[i] = fastReader.intNext();
+        }
+
+        long[][][] dp = new long[count][count][2];
+
+        for (int i = count - 1; i >= 0; i--) {
+            for (int j = i; j < count; j++) {
+                long[] curr;
+                if (i == j) {
+                    curr = new long[]{arr[i], 0};
+                } else {
+                    if (arr[i] + dp[i + 1][j][1] > arr[j] + dp[i][j - 1][1]) {
+                        curr = new long[]{arr[i] + dp[i + 1][j][1], dp[i + 1][j][0]};
+                    } else {
+                        curr = new long[]{arr[j] + dp[i][j - 1][1], dp[i][j - 1][0]};
+                    }
+                }
+                dp[i][j] = curr;
+            }
+        }
+
+        print(dp[0][count - 1][0]);
+    }
+
+    private static void solve2() throws IOException {
+        int count = fastReader.intNext();
+        long[] arr = new long[count];
+
+        for (int i = 0; i < count; i++) {
+            arr[i] = fastReader.intNext();
+        }
+
+        long[][] dp = new long[count][count];
+
+        for (int len = 1; len <= count; len++) {
+
+            for (int start = 0; start + len <= count; start++) {
+                int end = start + len - 1;
+
+                long x = (end >= start + 2) ? dp[start + 2][end] : 0;
+                long y = (end - 1 >= start + 1) ? dp[start + 1][end - 1] : 0;
+                long z = (end - 2 >= start) ? dp[start][end - 2] : 0;
+
+                dp[start][end] = Math.max(Math.min(x, y) + arr[start], Math.min(y, z) + arr[end]);
+            }
+        }
+
+        print(dp[0][count - 1]);
+
+    }
+
+
+    /************************************************************************************************************************************************/
+    public static void main(String[] args) throws IOException {
+
+        // solve();
+        solve2();
+        out.close();
+    }
+
+
+    static class Reader {
+        private byte[] buf = new byte[1024];
+        private int index;
+        private InputStream in;
+        private int total;
+
+        public Reader() {
+            in = System.in;
+        }
+
+        public int scan() throws IOException {
+            if (total < 0)
+                throw new InputMismatchException();
+            if (index >= total) {
+                index = 0;
+                total = in.read(buf);
+                if (total <= 0)
+                    return -1;
+            }
+            return buf[index++];
+        }
+
+        public int intNext() throws IOException {
+            int integer = 0;
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            int neg = 1;
+            if (n == '-') {
+                neg = -1;
+                n = scan();
+            }
+            while (!isWhiteSpace(n)) {
+                if (n >= '0' && n <= '9') {
+                    integer *= 10;
+                    integer += n - '0';
+                    n = scan();
+                } else throw new InputMismatchException();
+            }
+            return neg * integer;
+        }
+
+        public double doubleNext() throws IOException {
+            double doub = 0;
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            int neg = 1;
+            if (n == '-') {
+                neg = -1;
+                n = scan();
+            }
+            while (!isWhiteSpace(n) && n != '.') {
+                if (n >= '0' && n <= '9') {
+                    doub *= 10;
+                    doub += n - '0';
+                    n = scan();
+                } else throw new InputMismatchException();
+            }
+            if (n == '.') {
+                n = scan();
+                double temp = 1;
+                while (!isWhiteSpace(n)) {
+                    if (n >= '0' && n <= '9') {
+                        temp /= 10;
+                        doub += (n - '0') * temp;
+                        n = scan();
+                    } else throw new InputMismatchException();
+                }
+            }
+            return doub * neg;
+        }
+
+        public String read() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            while (!isWhiteSpace(n)) {
+                sb.append((char) n);
+                n = scan();
+            }
+            return sb.toString();
+        }
+
+        private boolean isWhiteSpace(int n) {
+            if (n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1)
+                return true;
+            return false;
+        }
+    }
+
+    static void shuffle(int[] aa, int n) {
+        Random rand = new Random();
+        for (int i = 1; i < n; i++) {
+            int j = rand.nextInt(i + 1);
+            int tmp = aa[i];
+            aa[i] = aa[j];
+            aa[j] = tmp;
+        }
+    }
+
+    static void shuffle(int[][] aa, int n) {
+        Random rand = new Random();
+        for (int i = 1; i < n; i++) {
+            int j = rand.nextInt(i + 1);
+            int first = aa[i][0];
+            int second = aa[i][1];
+            aa[i][0] = aa[j][0];
+            aa[i][1] = aa[j][1];
+            aa[j][0] = first;
+            aa[j][1] = second;
+        }
+    }
+
+    /**
+     * Tree Multiset utility class *
+     */
+    static class TMultiset<T extends Number> extends TreeMap<T, Integer> {
+        private int size = 0;
+
+        void add(T value) {
+            Integer count = get(value);
+            size++;
+            if (count == null) {
+                put(value, 1);
+            } else {
+                put(value, count + 1);
+            }
+        }
+
+        @SuppressWarnings(value = "unchecked")
+        @Override
+        public Integer remove(Object key) {
+            if (!containsKey(key)) {
+                return null;
+            }
+
+            size--;
+
+            Integer value = get(key);
+            if (value > 1) {
+                return put((T) key, value - 1);
+            }
+
+            return super.remove(key);
+        }
+
+        @java.lang.Override
+        public int size() {
+            return size;
+        }
+    }
+
+    /**
+     * It is a HashMap
+     */
+    static class HMap<T> extends HashMap<T, Integer> {
+        void add(T key) {
+            Integer count = get(key);
+            put(key, count == null ? 1 : count + 1);
+        }
+
+        @SuppressWarnings(value = "unchecked")
+        @Override
+        public Integer remove(Object key) {
+            if (!containsKey(key)) return null;
+
+            int count = get(key);
+            if (count > 1) return put((T) key, count - 1);
+
+            return super.remove(key);
+        }
+    }
+
+    static final class Comparators {
+        public static final Comparator<int[]> pairIntArr =
+                (x, y) -> x[0] == y[0] ? compare(x[1], y[1]) : compare(x[0], y[0]);
+
+        private static final int compare(final int x, final int y) {
+            return Integer.compare(x, y);
+        }
+    }
+
+
+    static void print(Object object) {
+        out.print(object);
+    }
+
+    static void println(Object object) {
+        out.println(object);
+    }
+}
+
+
