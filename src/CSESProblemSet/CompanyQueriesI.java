01b2519ab9666430dd65dcd639409bb7b9e2aeec
DavinderSinghKharoud <Dskharoud2@gmail.com>
2021-02-05 01:37:28 -0500

    Company Queries I(Binary Lifting)

---
+++ src/CSESProblemSet/CompanyQueriesI.java
@@ -0,0 +1,302 @@
+import java.io.*;
+import java.util.*;
+
+@SuppressWarnings("unchecked")
+public class CompanyQueriesI {
+	
+	int nxM = (int)2e5;
+	int[][] dp = new int[nxM][20];
+	int[] parent = new int[nxM];
+    void solve() throws IOException {
+
+		int n = read.intNext(), q = read.intNext();
+        for (int i = 0; i < 20; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+		for(int i = 1; i < n; i++ ){
+			parent[i] = read.intNext() - 1;
+			dp[i][0] = parent[i];
+		}
+
+
+
+		for(int i = 1; i < 20; i++ ){
+			
+			for(int node = 1; node < n; node++ ){
+				dp[node][i] = (dp[node][i - 1] == - 1)? -1: dp[ dp[node][i - 1]][i - 1];
+			}
+		}
+		
+		while( q--> 0 ){
+			int src = read.intNext() - 1, k = read.intNext();
+			
+			if( src == 0 && k > 1){
+				println(-1);
+			}else{
+				
+				for(int i = 19; i >= 0; i-- ){
+					if( ((k >> i) & 1 ) != 0 ){
+						src = dp[src][i];
+						if(src == - 1) break;
+					}
+				}
+
+				sbr.append((src == -1)?-1: src + 1).append(" ");
+
+			}
+		}
+		println(sbr);
+		
+    }
+
+
+    /************************************************************************************************************************************************/
+    public static void main(String[] args) throws IOException {
+
+        CompanyQueriesI object = new CompanyQueriesI();
+        object.solve();
+        out.close();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Reader read = new Reader();
+    static StringBuilder sbr = new StringBuilder();
+    static int mod = (int) 1e9 + 7;
+    static int dmax = Integer.MAX_VALUE;
+    static long lmax = Long.MAX_VALUE;
+    static int dmin = Integer.MIN_VALUE;
+    static long lmin = Long.MIN_VALUE;
+
+    static class Reader {
+        private byte[] buf = new byte[1024];
+        private int index;
+        private InputStream in;
+        private int total;
+
+        public Reader() {
+            in = System.in;
+        }
+
+        public int scan() throws IOException {
+            if (total < 0)
+                throw new InputMismatchException();
+            if (index >= total) {
+                index = 0;
+                total = in.read(buf);
+                if (total <= 0)
+                    return -1;
+            }
+            return buf[index++];
+        }
+
+        public int intNext() throws IOException {
+            int integer = 0;
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            int neg = 1;
+            if (n == '-') {
+                neg = -1;
+                n = scan();
+            }
+            while (!isWhiteSpace(n)) {
+                if (n >= '0' && n <= '9') {
+                    integer *= 10;
+                    integer += n - '0';
+                    n = scan();
+                } else throw new InputMismatchException();
+            }
+            return neg * integer;
+        }
+
+        public double doubleNext() throws IOException {
+            double doub = 0;
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            int neg = 1;
+            if (n == '-') {
+                neg = -1;
+                n = scan();
+            }
+            while (!isWhiteSpace(n) && n != '.') {
+                if (n >= '0' && n <= '9') {
+                    doub *= 10;
+                    doub += n - '0';
+                    n = scan();
+                } else throw new InputMismatchException();
+            }
+            if (n == '.') {
+                n = scan();
+                double temp = 1;
+                while (!isWhiteSpace(n)) {
+                    if (n >= '0' && n <= '9') {
+                        temp /= 10;
+                        doub += (n - '0') * temp;
+                        n = scan();
+                    } else throw new InputMismatchException();
+                }
+            }
+            return doub * neg;
+        }
+
+        public String read() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            while (!isWhiteSpace(n)) {
+                sb.append((char) n);
+                n = scan();
+            }
+            return sb.toString();
+        }
+
+        private boolean isWhiteSpace(int n) {
+            if (n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1)
+                return true;
+            return false;
+        }
+    }
+
+    static void shuffle(int[] aa, int n) {
+        Random rand = new Random();
+        for (int i = 1; i < n; i++) {
+            int j = rand.nextInt(i + 1);
+            int tmp = aa[i];
+            aa[i] = aa[j];
+            aa[j] = tmp;
+        }
+    }
+
+    static void shuffle(int[][] aa, int n) {
+        Random rand = new Random();
+        for (int i = 1; i < n; i++) {
+            int j = rand.nextInt(i + 1);
+            int first = aa[i][0];
+            int second = aa[i][1];
+            aa[i][0] = aa[j][0];
+            aa[i][1] = aa[j][1];
+            aa[j][0] = first;
+            aa[j][1] = second;
+        }
+    }
+
+    /**
+     * Tree Multiset utility class *
+     */
+    static class TMultiset<T extends Number> extends TreeMap<T, Integer> {
+        private int size = 0;
+
+        void add(T value) {
+            Integer count = get(value);
+            size++;
+            if (count == null) {
+                put(value, 1);
+            } else {
+                put(value, count + 1);
+            }
+        }
+
+        @SuppressWarnings(value = "unchecked")
+        @Override
+        public Integer remove(Object key) {
+            if (!containsKey(key)) {
+                return null;
+            }
+
+            size--;
+
+            Integer value = get(key);
+            if (value > 1) {
+                return put((T) key, value - 1);
+            }
+
+            return super.remove(key);
+        }
+
+        @java.lang.Override
+        public int size() {
+            return size;
+        }
+    }
+
+    /**
+     * It is a HashMap
+     */
+    static class HMap<T> extends HashMap<T, Integer> {
+        void add(T key) {
+            Integer count = get(key);
+            put(key, count == null ? 1 : count + 1);
+        }
+
+        @SuppressWarnings(value = "unchecked")
+        @Override
+        public Integer remove(Object key) {
+            if (!containsKey(key)) return null;
+
+            int count = get(key);
+            if (count > 1) return put((T) key, count - 1);
+
+            return super.remove(key);
+        }
+    }
+
+    static final class Comparators {
+        public static final Comparator<int[]> pairIntArr =
+                (x, y) -> x[0] == y[0] ? compare(x[1], y[1]) : compare(x[0], y[0]);
+
+        private static final int compare(final int x, final int y) {
+            return Integer.compare(x, y);
+        }
+    }
+
+
+    static void print(Object object) {
+        out.print(object);
+    }
+
+    static void println(Object object) {
+        out.println(object);
+    }
+
+
+    static long min(Long... objects) {
+        long min = Integer.MAX_VALUE;
+
+        for (Long num : objects) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+
+    static int min(Integer... objects) {
+        int min = Integer.MAX_VALUE;
+
+        for (Integer num : objects) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+
+    static long max(Long... objects) {
+        long max = Long.MIN_VALUE;
+
+        for (Long num : objects) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    static int max(Integer... objects) {
+        int max = Integer.MIN_VALUE;
+
+        for (Integer num : objects) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+}
+
+
