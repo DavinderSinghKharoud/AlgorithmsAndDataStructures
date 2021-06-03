e270dfd5356379153e1449d5656c2db44a6683a3
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-06-02 20:09:59 -0400

    Word equals

---
+++ src/Algorithms/LeetCode/WeeklyContest243/WordEquals.java
@@ -0,0 +1,31 @@
+package Algorithms.LeetCode.WeeklyContest243;
+
+import java.util.*;
+
+public class WordEquals {
+   public static void main(String[] args) {
+
+   }
+
+   public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+      int first, second, third;
+      StringBuilder sbr = new StringBuilder();
+      for (int i = 0; i < firstWord.length(); i++) {
+         sbr.append(firstWord.charAt(i) - 'a');
+      }
+      first = Integer.parseInt(sbr.toString());
+      sbr = new StringBuilder();
+      for (int i = 0; i < secondWord.length(); i++) {
+         sbr.append(secondWord.charAt(i) - 'a');
+      }
+      second = Integer.parseInt(sbr.toString());
+      sbr = new StringBuilder();
+
+      for (int i = 0; i < targetWord.length(); i++) {
+         sbr.append(targetWord.charAt(i) - 'a');
+      }
+
+      third = Integer.parseInt(sbr.toString());
+      return first + second == third;
+   }
+}
