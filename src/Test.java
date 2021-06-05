0523e368ed4137863790cf6592aedbe211210578
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-06-05 11:54:39 -0400

    Humongous Tree

--- src/Test.java
+++ src/Test.java
@@ -1,42 +1,11 @@
 public class Test {
 
     public static void main(String[] args) {
-
-        // value a, b, and c
-        double a = 1, b = 5, c = -20;
-        double root1, root2;
-
-        // calculate the determinant (b2 - 4ac)
-        double determinant = b * b - 4 * a * c;
-
-        // check if determinant is greater than 0
-        if (determinant > 0) {
-
-            // two real and distinct roots
-            root1 = (-b + Math.sqrt(determinant)) / (2 * a);
-            root2 = (-b - Math.sqrt(determinant)) / (2 * a);
-
-            System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+        for(int i = 11 ;i < (int)1e6; i += 11){
+            System.out.print("");
         }
-
-        // check if determinant is equal to 0
-        else if (determinant == 0) {
-
-            // two real and equal roots
-            // determinant is equal to 0
-            // so -b + 0 == -b
-            root1 = root2 = -b / (2 * a);
-            System.out.format("root1 = root2 = %.2f;", root1);
-        }
-
-        // if determinant is less than zero
-        else {
-
-            // roots are complex number and distinct
-            double real = -b / (2 * a);
-            double imaginary = Math.sqrt(-determinant) / (2 * a);
-            System.out.format("root1 = %.2f+%.2fi", real, imaginary);
-            System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
-        }
+        System.out.println("Finished");
     }
+
 }
+
