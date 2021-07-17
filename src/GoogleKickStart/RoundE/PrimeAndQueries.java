f7881d84c26026d5b07d224e74cdc52fee763697
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-07-16 23:01:53 -0400

    Prime And Queries

---
+++ src/GoogleKickStart/RoundE/PrimeAndQueries.java
@@ -0,0 +1,358 @@
+package GoogleKickStart.RoundE;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class PrimeAndQueries implements Runnable {
+
+    void solve() {
+        int t = ri();
+        for (int tt = 1; tt <= t; tt++) {
+            print("Case #" + tt + ": ");
+            int n = ri(), q = ri(), p = ri();
+            // Maintain 4 seg trees
+            Map<Integer, SegmentTree> map = new HashMap<>();
+
+            int[] arr = iArr(n);
+            for (int j = 0; j < n; j++) {
+                arr[j] = ri();
+            }
+            for (int i = 1; i <= 4; i++) {
+                long[] modArr = lArr(n);
+                for (int j = 0; j < n; j++) {
+                    modArr[j] = modVal(arr[j], i, p);
+                }
+                map.put(i, new SegmentTree(modArr));
+            }
+
+            for (int i = 0; i < q; i++) {
+                int type = ri();
+                if (type == 1) {
+                    int pos = ri() - 1, val = ri();
+                    // update all seg trees
+                    for (int j = 1; j <= 4; j++) {
+                        SegmentTree seg = map.get(j);
+                        seg.update(pos, modVal(val, j, p));
+                    }
+                } else {
+                    // Get the ans
+                    int s = ri(), l = ri() - 1, r = ri() - 1;
+                    print(map.get(s).query(l, r) + " ");
+                }
+            }
+            println("");
+        }
+    }
+
+    long modVal(int val, int n, int p) {
+        long pow = getVal(val, n);
+        long sec = getVal(val % p, n);
+        long diff = pow - sec;
+
+        long nearest = 1;
+        long div = p;
+        //Find the nearest div power
+        while (diff % div == 0 && div <= diff) {
+            nearest++;
+            div *= p;
+        }
+        return nearest - 1;
+    }
+
+    long getVal(int num, int n) {
+        long res = 1;
+        for (int i = 0; i < n; i++) {
+            res *= num;
+        }
+        return res;
+    }
+
+    static class SegmentTree {
+
+        long[] seg;
+        long[] arr;
+        int len;
+
+        public SegmentTree(long[] arr) {
+            len = arr.length;
+            this.arr = arr;
+            seg = new long[len << 1];
+            construct();
+        }
+
+        private void construct() {
+
+            // Assign values to leaves of the segment Tree
+            for (int i = 0; i < len; i++) {
+                seg[len + i] = arr[i];
+            }
+
+            // Compute sum
+            for (int i = len - 1; i >= 1; i--) {
+                int pos = i << 1;
+                seg[i] = seg[pos] + seg[pos + 1];
+            }
+        }
+
+        public void update(int target, long value) {
+            target += len;
+
+            seg[target] = value;
+
+            while (target > 1) {
+                // Move up by one level
+                target >>= 1;
+                int pos = target << 1;
+                seg[target] = seg[pos] + seg[pos + 1];
+            }
+
+        }
+
+        public long query(int l, int r) {
+            l += len;
+            r += len;
+
+            long res = 0;
+            while (l <= r) {
+
+                // If left index is odd
+                if ((l & 1) == 1) {
+                    res += seg[l];
+                    l++; // make it even
+                }
+
+                // If right index is even
+                if ((r & 1) == 0) {
+                    res += seg[r];
+                    r--;
+                }
+
+                // Move to the next higher level
+                l >>= 1;
+                r >>= 1;
+            }
+            return res;
+        }
+
+    }
+
+    /************************************************************************************************************************************************/
+    public static void main(String[] args) throws IOException {
+        new Thread(null, new PrimeAndQueries(), "1").start();
+    }
+
+    static PrintWriter out = new PrintWriter(System.out);
+    static Reader read = new Reader();
+    static StringBuilder sbr = new StringBuilder();
+    static int mod = (int) 1e9 + 7;
+    static int dmax = Integer.MAX_VALUE;
+    static long lmax = Long.MAX_VALUE;
+    static int dmin = Integer.MIN_VALUE;
+    static long lmin = Long.MIN_VALUE;
+    static int[] dprimes = new int[]{1, 11, 101, 1087, 99991, 100001, 1000003, 15485863, 999999937};
+
+    @Override
+    public void run() {
+        solve();
+        out.close();
+    }
+
+    static class Reader {
+        private byte[] buf = new byte[1024];
+        private int index;
+        private InputStream in;
+        private int total;
+
+        public Reader() {
+            in = System.in;
+        }
+
+        public int scan() throws IOException {
+            if (total < 0)
+                throw new InputMismatchException();
+            if (index >= total) {
+                index = 0;
+                total = in.read(buf);
+                if (total <= 0)
+                    return -1;
+            }
+            return buf[index++];
+        }
+
+        public int intNext() throws IOException {
+            int integer = 0;
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            int neg = 1;
+            if (n == '-') {
+                neg = -1;
+                n = scan();
+            }
+            while (!isWhiteSpace(n)) {
+                if (n >= '0' && n <= '9') {
+                    integer *= 10;
+                    integer += n - '0';
+                    n = scan();
+                } else
+                    throw new InputMismatchException();
+            }
+            return neg * integer;
+        }
+
+        public double doubleNext() throws IOException {
+            double doub = 0;
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            int neg = 1;
+            if (n == '-') {
+                neg = -1;
+                n = scan();
+            }
+            while (!isWhiteSpace(n) && n != '.') {
+                if (n >= '0' && n <= '9') {
+                    doub *= 10;
+                    doub += n - '0';
+                    n = scan();
+                } else
+                    throw new InputMismatchException();
+            }
+            if (n == '.') {
+                n = scan();
+                double temp = 1;
+                while (!isWhiteSpace(n)) {
+                    if (n >= '0' && n <= '9') {
+                        temp /= 10;
+                        doub += (n - '0') * temp;
+                        n = scan();
+                    } else
+                        throw new InputMismatchException();
+                }
+            }
+            return doub * neg;
+        }
+
+        public String read() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int n = scan();
+            while (isWhiteSpace(n))
+                n = scan();
+            while (!isWhiteSpace(n)) {
+                sb.append((char) n);
+                n = scan();
+            }
+            return sb.toString();
+        }
+
+        private boolean isWhiteSpace(int n) {
+            if (n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1)
+                return true;
+            return false;
+        }
+    }
+
+    // Gives strict lowerBound that previous number would be smaller than the target
+    int lowerBound(int[] arr, int val) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = (r + l) >> 1;
+            if (arr[mid] >= val) {
+                r = mid;
+            } else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    // Gives strict upperBound that next number would be greater than the target
+    int upperBound(int[] arr, int val) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = (r + l + 1) >> 1;
+            if (arr[mid] <= val) {
+                l = mid;
+            } else
+                r = mid - 1;
+        }
+        return l;
+    }
+
+    static void print(Object object) {
+        out.print(object);
+    }
+
+    static void println(Object object) {
+        out.println(object);
+    }
+
+    static int[] iArr(int len) {
+        return new int[len];
+    }
+
+    static long[] lArr(int len) {
+        return new long[len];
+    }
+
+    static long min(long a, long b) {
+        return Math.min(a, b);
+    }
+
+    static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    static long max(Long a, Long b) {
+        return Math.max(a, b);
+    }
+
+    static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    static int ri() {
+        try {
+            return read.intNext();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(2);
+        return -1;
+    }
+
+    static long rl() {
+        try {
+            return Long.parseLong(read.read());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(2);
+        return -1;
+    }
+
+    static String rs() {
+        try {
+            return read.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(2);
+        return "";
+    }
+
+    static char rc() {
+        return rs().charAt(0);
+    }
+
+    static double rd() {
+        try {
+            return read.doubleNext();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(2);
+        return -1;
+    }
+}
