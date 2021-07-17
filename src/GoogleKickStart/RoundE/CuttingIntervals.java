1840b01585420fa1f59be0b0627df0e62fa18689
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-07-16 22:59:36 -0400

    Cutting Intervals

---
+++ src/GoogleKickStart/RoundE/CuttingIntervals.java
@@ -0,0 +1,294 @@
+package GoogleKickStart.RoundE;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class CuttingIntervals implements Runnable {
+
+   void solve() {
+      int t = ri();
+      for (int tt = 1; tt <= t; tt++) {
+         print("Case #" + tt + ": ");
+         int n = ri();
+         long c = rl();
+         Map<Long, Long> map = new HashMap<>();
+         for (int i = 0; i < n; i++) {
+            // For start + 1, increment count
+            // For end, decrement count, to get info about how many intervals have this
+            // number
+            long a = rl(), b = rl();
+            map.put(a + 1, map.getOrDefault(a + 1, 0L) + 1);
+            map.put(b, map.getOrDefault(b, 0L) - 1);
+         }
+         long res = n;
+         long prev = -1, count = 0;
+         long[][] mapKeys = new long[map.size()][2];
+         int index = 0;
+         for (Long key : map.keySet()) {
+            mapKeys[index++] = new long[] { key, map.get(key) };
+         }
+         shuffle(mapKeys);
+         Arrays.sort(mapKeys, Comparator.comparingLong(o -> o[0]));
+         long[][] contributions = new long[mapKeys.length - 1][2];
+         index = 0;
+         for (long[] data : mapKeys) {
+            long key = data[0], val = data[1];
+            if (prev == -1) {
+               prev = key;
+               count = val;
+            } else {
+               long numbers = key - prev;
+               contributions[index++] = new long[] { numbers, count };
+               prev = key;
+               count += val;
+            }
+         }
+
+         shuffle(contributions);
+         Arrays.sort(contributions, Comparator.comparingLong(o -> o[1]));
+
+         for (int i = contributions.length - 1; i >= 0; i--) {
+            long needed = min(contributions[i][0], c);
+            res += needed * contributions[i][1];
+            c -= needed;
+            if (c == 0)
+               break;
+         }
+         println(res);
+      }
+   }
+
+   static void shuffle(long[][] aa) {
+      int n = aa.length;
+      Random rand = new Random();
+      for (int i = 1; i < n; i++) {
+         int j = rand.nextInt(i + 1);
+         long first = aa[i][0];
+         long second = aa[i][1];
+         aa[i][0] = aa[j][0];
+         aa[i][1] = aa[j][1];
+         aa[j][0] = first;
+         aa[j][1] = second;
+      }
+   }
+
+   /************************************************************************************************************************************************/
+   public static void main(String[] args) throws IOException {
+      new Thread(null, new CuttingIntervals(), "1").start();
+   }
+
+   static PrintWriter out = new PrintWriter(System.out);
+   static Reader read = new Reader();
+   static StringBuilder sbr = new StringBuilder();
+   static int mod = (int) 1e9 + 7;
+   static int dmax = Integer.MAX_VALUE;
+   static long lmax = Long.MAX_VALUE;
+   static int dmin = Integer.MIN_VALUE;
+   static long lmin = Long.MIN_VALUE;
+   static int[] dprimes = new int[] { 1, 11, 101, 1087, 99991, 100001, 1000003, 15485863, 999999937 };
+
+   @Override
+   public void run() {
+      solve();
+      out.close();
+   }
+
+   static class Reader {
+      private byte[] buf = new byte[1024];
+      private int index;
+      private InputStream in;
+      private int total;
+
+      public Reader() {
+         in = System.in;
+      }
+
+      public int scan() throws IOException {
+         if (total < 0)
+            throw new InputMismatchException();
+         if (index >= total) {
+            index = 0;
+            total = in.read(buf);
+            if (total <= 0)
+               return -1;
+         }
+         return buf[index++];
+      }
+
+      public int intNext() throws IOException {
+         int integer = 0;
+         int n = scan();
+         while (isWhiteSpace(n))
+            n = scan();
+         int neg = 1;
+         if (n == '-') {
+            neg = -1;
+            n = scan();
+         }
+         while (!isWhiteSpace(n)) {
+            if (n >= '0' && n <= '9') {
+               integer *= 10;
+               integer += n - '0';
+               n = scan();
+            } else
+               throw new InputMismatchException();
+         }
+         return neg * integer;
+      }
+
+      public double doubleNext() throws IOException {
+         double doub = 0;
+         int n = scan();
+         while (isWhiteSpace(n))
+            n = scan();
+         int neg = 1;
+         if (n == '-') {
+            neg = -1;
+            n = scan();
+         }
+         while (!isWhiteSpace(n) && n != '.') {
+            if (n >= '0' && n <= '9') {
+               doub *= 10;
+               doub += n - '0';
+               n = scan();
+            } else
+               throw new InputMismatchException();
+         }
+         if (n == '.') {
+            n = scan();
+            double temp = 1;
+            while (!isWhiteSpace(n)) {
+               if (n >= '0' && n <= '9') {
+                  temp /= 10;
+                  doub += (n - '0') * temp;
+                  n = scan();
+               } else
+                  throw new InputMismatchException();
+            }
+         }
+         return doub * neg;
+      }
+
+      public String read() throws IOException {
+         StringBuilder sb = new StringBuilder();
+         int n = scan();
+         while (isWhiteSpace(n))
+            n = scan();
+         while (!isWhiteSpace(n)) {
+            sb.append((char) n);
+            n = scan();
+         }
+         return sb.toString();
+      }
+
+      private boolean isWhiteSpace(int n) {
+         if (n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1)
+            return true;
+         return false;
+      }
+   }
+
+   // Gives strict lowerBound that previous number would be smaller than the target
+   int lowerBound(int[] arr, int val) {
+      int l = 0, r = arr.length - 1;
+      while (l < r) {
+         int mid = (r + l) >> 1;
+         if (arr[mid] >= val) {
+            r = mid;
+         } else
+            l = mid + 1;
+      }
+      return l;
+   }
+
+   // Gives strict upperBound that next number would be greater than the target
+   int upperBound(int[] arr, int val) {
+      int l = 0, r = arr.length - 1;
+      while (l < r) {
+         int mid = (r + l + 1) >> 1;
+         if (arr[mid] <= val) {
+            l = mid;
+         } else
+            r = mid - 1;
+      }
+      return l;
+   }
+
+   static void print(Object object) {
+      out.print(object);
+   }
+
+   static void println(Object object) {
+      out.println(object);
+   }
+
+   static int[] iArr(int len) {
+      return new int[len];
+   }
+
+   static long[] lArr(int len) {
+      return new long[len];
+   }
+
+   static long min(long a, long b) {
+      return Math.min(a, b);
+   }
+
+   static int min(int a, int b) {
+      return Math.min(a, b);
+   }
+
+   static long max(Long a, Long b) {
+      return Math.max(a, b);
+   }
+
+   static int max(int a, int b) {
+      return Math.max(a, b);
+   }
+
+   static int ri() {
+      try {
+         return read.intNext();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return -1;
+   }
+
+   static long rl() {
+      try {
+         return Long.parseLong(read.read());
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return -1;
+   }
+
+   static String rs() {
+      try {
+         return read.read();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return "";
+   }
+
+   static char rc() {
+      return rs().charAt(0);
+   }
+
+   static double rd() {
+      try {
+         return read.doubleNext();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return -1;
+   }
+}
