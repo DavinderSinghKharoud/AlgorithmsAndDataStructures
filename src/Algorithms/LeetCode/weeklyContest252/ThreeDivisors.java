886faff0a7ab627fe35501ba8bee237aaef6da40
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-08-04 23:23:26 -0400

    Three Divisors

---
+++ src/Algorithms/LeetCode/weeklyContest252/ThreeDivisors.java
@@ -0,0 +1,16 @@
+package Algorithms.LeetCode.weeklyContest252;
+
+public class ThreeDivisors {
+   public static void main(String[] args) {
+      ThreeDivisors o = new ThreeDivisors();
+   }
+
+   public boolean isThree(int n) {
+      int count = 0;
+      for (int i = 1; i <= n; i++) {
+         if (n % i == 0)
+            count++;
+      }
+      return count == 3;
+   }
+}
