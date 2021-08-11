38a08a6dee9fe9b4d3907f92ddd51a75855dbca0
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-08-10 21:51:57 -0400

    Remove Stones Minimize

---
+++ src/Algorithms/LeetCode/RemoveStonesMinimize.java
@@ -0,0 +1,27 @@
+package Algorithms.LeetCode;
+
+import java.util.*;
+
+public class RemoveStonesMinimize {
+
+   public static void main(String[] args) {
+
+   }
+
+   public int minStoneSum(int[] piles, int k) {
+      PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+      long sum = 0;
+      for (int pile : piles) {
+         sum += pile;
+         pq.add(pile);
+      }
+      while (k-- > 0 && !pq.isEmpty()) {
+         int curr = pq.poll();
+         sum -= curr;
+         int val = (int) Math.ceil((double) curr / 2);
+         sum += val;
+         pq.add(val);
+      }
+      return (int) sum;
+   }
+}
