e9df7f63c0ee13faccae9a6ca149086364e18390
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-08-20 23:45:09 -0400

    Two Tables

---
+++ CodeForces/TwoTables.java
@@ -0,0 +1,261 @@
+package CodeForces;
+
+import java.io.*;
+import java.util.*;
+
+public class TwoTables implements Runnable {
+
+   void solve() {
+      int t = ri();
+      while (t-- > 0) {
+         long W = rl(), H = rl();
+         long x1 = rl(), y1 = rl(), x2 = rl(), y2 = rl();
+         long w = rl(), h = rl();
+
+         // It is possible by some way
+         long ans = lmax;
+         if (h + (y2 - y1) <= H) {
+            // Bottom
+            if (y1 < h) {
+               ans = h - y1;
+            } else
+               ans = 0;
+
+            // Up
+            if (y2 > (H - h)) {
+               ans = min(ans, y2 - (H - h));
+            } else
+               ans = 0;
+         }
+
+         if (w + (x2 - x1) <= W) {
+            // Left
+            if (x1 < w) {
+               ans = min(ans, w - x1);
+            } else
+               ans = 0;
+
+            // Right
+            if ((W - w) < x2) {
+               ans = min(ans, x2 - (W - w));
+            } else
+               ans = 0;
+         }
+
+         println((ans != lmax) ? ans * 1D : -1);
+      }
+
+   }
+
+   /************************************************************************************************************************************************/
+   public static void main(String[] args) throws IOException {
+      new Thread(null, new TwoTables(), "1").start();
+   }
+
+   static PrintWriter out = new PrintWriter(System.out);
+   static Reader read = new Reader();
+   static StringBuilder sbr = new StringBuilder();
+   static int mod = (int) 1e9 + 7;
+   static int dmax = Integer.MAX_VALUE;
+   static long lmax = Long.MAX_VALUE;
+   static int dmin = Integer.MIN_VALUE;
+   static long lmin = Long.MIN_VALUE;
+   static int[] dprimes = new int[] { 1, 11, 101, 1087, 99991, 100001, 1000003, 15485863, 999999937 };
+
+   @Override
+   public void run() {
+      solve();
+      out.close();
+   }
+
+   static class Reader {
+      private byte[] buf = new byte[1024];
+      private int index;
+      private InputStream in;
+      private int total;
+
+      public Reader() {
+         in = System.in;
+      }
+
+      public int scan() throws IOException {
+         if (total < 0)
+            throw new InputMismatchException();
+         if (index >= total) {
+            index = 0;
+            total = in.read(buf);
+            if (total <= 0)
+               return -1;
+         }
+         return buf[index++];
+      }
+
+      public int intNext() throws IOException {
+         int integer = 0;
+         int n = scan();
+         while (isWhiteSpace(n))
+            n = scan();
+         int neg = 1;
+         if (n == '-') {
+            neg = -1;
+            n = scan();
+         }
+         while (!isWhiteSpace(n)) {
+            if (n >= '0' && n <= '9') {
+               integer *= 10;
+               integer += n - '0';
+               n = scan();
+            } else
+               throw new InputMismatchException();
+         }
+         return neg * integer;
+      }
+
+      public double doubleNext() throws IOException {
+         double doub = 0;
+         int n = scan();
+         while (isWhiteSpace(n))
+            n = scan();
+         int neg = 1;
+         if (n == '-') {
+            neg = -1;
+            n = scan();
+         }
+         while (!isWhiteSpace(n) && n != '.') {
+            if (n >= '0' && n <= '9') {
+               doub *= 10;
+               doub += n - '0';
+               n = scan();
+            } else
+               throw new InputMismatchException();
+         }
+         if (n == '.') {
+            n = scan();
+            double temp = 1;
+            while (!isWhiteSpace(n)) {
+               if (n >= '0' && n <= '9') {
+                  temp /= 10;
+                  doub += (n - '0') * temp;
+                  n = scan();
+               } else
+                  throw new InputMismatchException();
+            }
+         }
+         return doub * neg;
+      }
+
+      public String read() throws IOException {
+         StringBuilder sb = new StringBuilder();
+         int n = scan();
+         while (isWhiteSpace(n))
+            n = scan();
+         while (!isWhiteSpace(n)) {
+            sb.append((char) n);
+            n = scan();
+         }
+         return sb.toString();
+      }
+
+      public String rline() throws IOException {
+         StringBuilder sb = new StringBuilder();
+         int n = scan();
+         while (n != '\n') {
+            sb.append((char) n);
+            n = scan();
+         }
+         return sb.toString();
+      }
+
+      private boolean isWhiteSpace(int n) {
+         if (n == ' ' || n == '\n' || n == '\r' || n == '\t' || n == -1)
+            return true;
+         return false;
+      }
+   }
+
+   static void print(Object object) {
+      out.print(object);
+   }
+
+   static void println(Object object) {
+      out.println(object);
+   }
+
+   static int[] iArr(int len) {
+      return new int[len];
+   }
+
+   static long[] lArr(int len) {
+      return new long[len];
+   }
+
+   static long min(long a, long b) {
+      return Math.min(a, b);
+   }
+
+   static int min(int a, int b) {
+      return Math.min(a, b);
+   }
+
+   static long max(Long a, Long b) {
+      return Math.max(a, b);
+   }
+
+   static int max(int a, int b) {
+      return Math.max(a, b);
+   }
+
+   static int ri() {
+      try {
+         return read.intNext();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return -1;
+   }
+
+   static long rl() {
+      try {
+         return Long.parseLong(read.read());
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return -1;
+   }
+
+   static String rs() {
+      try {
+         return read.read();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return "";
+   }
+
+   static String rline() {
+      try {
+         return read.rline();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return "";
+   }
+
+   static char rc() {
+      return rs().charAt(0);
+   }
+
+   static double rd() {
+      try {
+         return read.doubleNext();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      System.exit(2);
+      return -1;
+   }
+}
