ac1f0cd8f0e9d69616b91ac948aea81e61eec047
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-09-09 23:04:53 -0400

    Count Quadruplets

---
+++ LeetCode/weeklyContest257/CountQuadruplets.java
@@ -0,0 +1,27 @@
+package LeetCode.weeklyContest257;
+
+public class CountQuadruplets {
+   public static void main(String[] args) {
+      CountQuadruplets o = new CountQuadruplets();
+       System.out.println(o.countQuadruplets(new int[]{35,15,38,1,10,26}));
+   }
+
+   public int countQuadruplets(int[] nums) {
+      int count = 0;
+      int len = nums.length;
+      for (int i = len - 1; i >= 0; i--) {
+         int first = nums[i];
+         for (int j = i - 1; j >= 0; j--) {
+            int second = first - nums[j];
+            for (int k = j - 1; k >= 0; k--) {
+               int third = second - nums[k];
+               for (int l = k - 1; l >= 0; l--) {
+                  if (third - nums[l] == 0)
+                     count++;
+               }
+            }
+         }
+      }
+      return count;
+   }
+}
