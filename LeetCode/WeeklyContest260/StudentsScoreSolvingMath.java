9cc21ffd01ecb8cb6d89b3ef14dc680756c2e8f6
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-09-28 00:12:37 -0400

    Student Score Solving Math

---
+++ LeetCode/WeeklyContest260/StudentsScoreSolvingMath.java
@@ -0,0 +1,8 @@
+package LeetCode.WeeklyContest260;
+
+public class StudentsScoreSolvingMath {
+   public static void main(String[] args) {
+      StudentsScoreSolvingMath o = new StudentsScoreSolvingMath();
+   }
+
+}
