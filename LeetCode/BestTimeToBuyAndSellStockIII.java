5d2c584915bc5fcf75a5a36726193938d03e3cde
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-10-07 23:56:56 -0400

    Best time to buy and sell stock IV

--- LeetCode/BestTimeToBuyAndSellStockIII.java
+++ LeetCode/BestTimeToBuyAndSellStockIII.java
@@ -3,7 +3,18 @@ package LeetCode;
 public class BestTimeToBuyAndSellStockIII {
 
     public static void main(String[] args) {
+        System.out.println(new BestTimeToBuyAndSellStockIII().maxProfit2(new int[]{3,3,5,0,0,3,1,4}));
+    }
 
+    public int maxProfit2(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, sell1 = 0, buy2 = Integer.MIN_VALUE, sell2 = 0;
+        for (int price : prices) {
+            buy1 = Math.max(buy1, -price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell2 = Math.max(sell2, buy2 + price);
+        }
+        return sell2;
     }
 
     public int maxProfit(int[] prices) {
@@ -17,7 +28,7 @@ public class BestTimeToBuyAndSellStockIII {
             min = Math.min(prices[i], min);
             left[i] = Math.max(left[i - 1], prices[i] - min);
         }
-        ans = 0;
+
         int max = prices[len - 1];
         for(int i = len - 2; i >= 0; i--){
             max = Math.max(max, prices[i]);
