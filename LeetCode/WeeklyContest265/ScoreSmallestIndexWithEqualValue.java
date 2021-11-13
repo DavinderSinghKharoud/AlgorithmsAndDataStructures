9450f82cc6361117de0845ef31ef502bf4e5d772
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-11-12 23:39:05 -0500

    Score smallest index with equal value

---
+++ LeetCode/WeeklyContest265/ScoreSmallestIndexWithEqualValue.java
@@ -0,0 +1,11 @@
+package LeetCode.WeeklyContest265;
+
+public class ScoreSmallestIndexWithEqualValue {
+   public static void main(String[] args) {
+      ScoreSmallestIndexWithEqualValue o = new ScoreSmallestIndexWithEqualValue();
+   }
+
+//   public int smallestEqual(int[] nums) {
+//
+//   }
+}
