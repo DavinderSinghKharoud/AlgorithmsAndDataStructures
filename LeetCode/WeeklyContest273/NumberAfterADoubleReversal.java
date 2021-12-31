09f75e5d504615469135e0563ace39ff56215cb4
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-12-30 20:28:44 -0500

    Number after a double reversal

---
+++ LeetCode/WeeklyContest273/NumberAfterADoubleReversal.java
@@ -0,0 +1,15 @@
+package LeetCode.WeeklyContest273;
+
+public class NumberAfterADoubleReversal {
+   public static void main(String[] args) {
+      NumberAfterADoubleReversal o = new NumberAfterADoubleReversal();
+
+   }
+
+//   public boolean isSameAfterReversals(int num) {
+//      StringBuilder sbr = new StringBuilder();
+//      sbr.append(num);
+//      int reverse = Integer.valueOf(sbr.reverse().toString());
+//
+//   }
+}
