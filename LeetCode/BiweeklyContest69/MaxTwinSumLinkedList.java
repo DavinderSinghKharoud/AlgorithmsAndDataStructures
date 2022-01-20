8deba1bd10ee58d20a36fc1d2822b59de1c3995d
DavinderSinghKharoud <dskharoud2@gmail.com>
2022-01-19 21:26:03 -0500

    Max twin sum linked list

---
+++ LeetCode/BiweeklyContest69/MaxTwinSumLinkedList.java
@@ -0,0 +1,7 @@
+package LeetCode.BiweeklyContest69;
+
+public class MaxTwinSumLinkedList {
+    public static void main(String[] args) {
+        MaxTwinSumLinkedList o = new MaxTwinSumLinkedList();
+    }
+}
