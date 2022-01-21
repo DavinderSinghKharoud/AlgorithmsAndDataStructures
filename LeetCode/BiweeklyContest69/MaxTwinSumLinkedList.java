302c24b83bd8590f2ef8fe058771039111cb87e1
DavinderSinghKharoud <dskharoud2@gmail.com>
2022-01-20 21:09:13 -0500

    Max running time n computers

--- LeetCode/BiweeklyContest69/MaxTwinSumLinkedList.java
+++ LeetCode/BiweeklyContest69/MaxTwinSumLinkedList.java
@@ -1,7 +1,26 @@
 package LeetCode.BiweeklyContest69;
 
 public class MaxTwinSumLinkedList {
-    public static void main(String[] args) {
-        MaxTwinSumLinkedList o = new MaxTwinSumLinkedList();
-    }
+   public static void main(String[] args) {
+      MaxTwinSumLinkedList o = new MaxTwinSumLinkedList();
+   }
+
+
+
+//   public int pairSum(ListNode head) {
+//      List<Integer> lst = new ArrayList<>();
+//      while (head != null) {
+//         lst.add(head.val);
+//         head = head.next;
+//      }
+//
+//      int max = 0;
+//      int start = 0, end = lst.size();
+//      while (start < end) {
+//         max = Math.max(max, lst.get(start) + lst.get(end));
+//         start++;
+//         end--;
+//      }
+//      return max;
+//   }
 }
