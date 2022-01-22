54ec68045638bf7337b10e90fbffbfdb31134bcd
DavinderSinghKharoud <dskharoud2@gmail.com>
2022-01-21 22:37:07 -0500

    Stamping the grid

---
+++ LeetCode/BiweeklyContest69/StampingTheGrid.java
@@ -0,0 +1,30 @@
+package LeetCode.BiweeklyContest69;
+
+import LeetCode.MiddleOfTheLinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StampingTheGrid {
+   public static void main(String[] args) {
+      StampingTheGrid o = new StampingTheGrid();
+   }
+
+//   public int pairSum(MiddleOfTheLinkedList.ListNode head) {
+//      List<Integer> lst = new ArrayList<>();
+//      while (head != null) {
+//         lst.add(head.val);
+//         head = head.next;
+//      }
+//
+//      int max = 0;
+//      int start = 0, end = lst.size();
+//      while (start < end) {
+//         max = Math.max(max, lst.get(start) + lst.get(end));
+//         start++;
+//         end--;
+//      }
+//      return max;
+//   }
+
+}
