e9b7d772665c620c3c69c9ded549b43733488106
DavinderSinghKharoud <dskharoud2@gmail.com>
2022-01-31 22:25:35 -0500

    Substring given hash

--- LeetCode/BiweeklyContest69/StampingTheGrid.java
+++ LeetCode/BiweeklyContest69/StampingTheGrid.java
@@ -1,101 +1,142 @@
 package LeetCode.BiweeklyContest69;
 
+/**
+ * You are given an m x n binary matrix grid where each cell is either 0 (empty) or 1 (occupied).
+ *
+ * You are then given stamps of size stampHeight x stampWidth. We want to fit the stamps such that they follow the given restrictions and requirements:
+ *
+ * Cover all the empty cells.
+ * Do not cover any of the occupied cells.
+ * We can put as many stamps as we want.
+ * Stamps can overlap with each other.
+ * Stamps are not allowed to be rotated.
+ * Stamps must stay completely inside the grid.
+ * Return true if it is possible to fit the stamps while following the given restrictions and requirements. Otherwise, return false.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0]], stampHeight = 4, stampWidth = 3
+ * Output: true
+ * Explanation: We have two overlapping stamps (labeled 1 and 2 in the image) that are able to cover all the empty cells.
+ * Example 2:
+ *
+ *
+ * Input: grid = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]], stampHeight = 2, stampWidth = 2
+ * Output: false
+ * Explanation: There is no way to fit the stamps onto all the empty cells without the stamps going outside the grid.
+ *
+ *
+ * Constraints:
+ *
+ * m == grid.length
+ * n == grid[r].length
+ * 1 <= m, n <= 105
+ * 1 <= m * n <= 2 * 105
+ * grid[r][c] is either 0 or 1.
+ * 1 <= stampHeight, stampWidth <= 105
+ * Accepted
+ * 2.5K
+ * Submissions
+ */
 public class StampingTheGrid {
-   public static void main(String[] args) {
-      StampingTheGrid o = new StampingTheGrid();
-      System.out.println(o.possibleToStamp(
-            new int[][] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } }, 4, 3));
-   }
+    public static void main(String[] args) {
+        StampingTheGrid o = new StampingTheGrid();
+        System.out.println(o.possibleToStamp(
+                new int[][]{{1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}
+                }, 4, 3));
+    }
 
-   int n, m, stampHeight, stampWidth;
+    int n, m, stampHeight, stampWidth;
 
-   public boolean possibleToStamp(int[][] grid, int stampHeight, int stampWidth) {
-      int n = 0;
+    public boolean possibleToStamp(int[][] grid, int stampHeight, int stampWidth) {
+        n = grid.length;
+        m = grid[0].length;
+        this.stampHeight = stampHeight;
+        this.stampWidth = stampWidth;
 
-      n = grid.length;
-      m = grid[0].length;
-      this.stampHeight = stampHeight;
-      this.stampWidth = stampWidth;
+        int[][] validStampMarkedGrid = markValidStampsBottomRightCorner(grid);
+        int[][] gridPrefixSum = get2DPrefixSum(validStampMarkedGrid);
 
-      int[][] validStampMarkedGrid = markValidStampsBottomRightCorner(grid);
-      int[][] gridPrefixSum = get2DPrefixSum(validStampMarkedGrid);
+        return isEveryEmptyCellCovered(gridPrefixSum, grid);
+    }
 
-      return isEveryEmptyCellCovered(gridPrefixSum, grid);
-   }
-
-   boolean isEveryEmptyCellCovered(int[][] gridPrefixSum, int[][] grid) {
-      for (int row = 0; row < n; row++) {
-         for (int col = 0; col < m; col++) {
-            if (gridPrefixSum[row][col] == 0 && grid[row][col] == 0)
-               return false;
-         }
-      }
-      return true;
-   }
-
-   int[][] markValidStampsBottomRightCorner(int[][] grid) {
-      int[][] markedGrid = new int[n][m];
-      int[][] gridPrefixSum = get2DPrefixSum(grid);
-      for (int row = 0; row <= n - stampHeight; row++) {
-         for (int col = 0; col <= m - stampWidth; col++) {
-            Point upperLeftCorner = new Point(row, col);
-            Point bottomRightCorner = new Point(row + stampHeight - 1, col + stampWidth - 1);
-
-            if (isGridEmpty(gridPrefixSum, upperLeftCorner, bottomRightCorner)) {
-               markPoint(markedGrid, bottomRightCorner);
+    boolean isEveryEmptyCellCovered(int[][] gridPrefixSum, int[][] grid) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (gridPrefixSum[row + 1][col + 1] == 0 && grid[row][col] == 0)
+                    return false;
             }
-         }
-      }
-      return markedGrid;
-   }
+        }
+        return true;
+    }
 
-   private void markPoint(int[][] markedGrid, Point point) {
-      markedGrid[point.x][point.y]++;
-      markedGrid[point.x + stampHeight - 1][point.y + stampWidth - 1]++;
-      markedGrid[point.x + stampHeight - 1][point.y]--;
-      markedGrid[point.x][point.y + stampWidth - 1]--;
-   }
+    int[][] markValidStampsBottomRightCorner(int[][] grid) {
+        int[][] markedGrid = new int[n + 1][m + 1];
+        int[][] gridPrefixSum = get2DPrefixSum(grid);
+        for (int row = 0; row <= n - stampHeight; row++) {
+            for (int col = 0; col <= m - stampWidth; col++) {
+                Point upperLeftCorner = new Point(row, col);
+                Point bottomRightCorner = new Point(row + stampHeight, col + stampWidth);
 
-   private int[][] get2DPrefixSum(int[][] grid) {
-      int[][] gridPrefixSum = new int[n][m];
-      for (int row = 0; row < n; row++) {
-         for (int col = 0; col < m; col++) {
-            gridPrefixSum[row][col] = grid[row][col] + get2DSum(gridPrefixSum, new Point(row - 1, col))
-                  + get2DSum(gridPrefixSum, new Point(row, col - 1))
-                  - get2DSum(gridPrefixSum, new Point(row - 1, col - 1));
-         }
-      }
-      return gridPrefixSum;
-   }
+                if (isGridEmpty(gridPrefixSum, upperLeftCorner, bottomRightCorner)) {
+                    markPoint(markedGrid, upperLeftCorner);
+                }
+            }
+        }
+        return markedGrid;
+    }
 
-   private boolean isGridEmpty(int[][] gridPrefixSum, Point upperLeftCorner, Point bottomRightCorner) {
-      return get2DSum(gridPrefixSum, upperLeftCorner, bottomRightCorner) == 0;
-   }
+    private void markPoint(int[][] markedGrid, Point point) {
+        markedGrid[point.x][point.y]++;
+        markedGrid[point.x + stampHeight][point.y + stampWidth]++;
+        markedGrid[point.x + stampHeight][point.y]--;
+        markedGrid[point.x][point.y + stampWidth]--;
+    }
 
-   int get2DSum(int[][] gridPrefixSum, Point upperLeftCorner, Point bottomRightCorner) {
-      int totalSum = gridPrefixSum[bottomRightCorner.x][bottomRightCorner.y];
-      totalSum -= get2DSum(gridPrefixSum, new Point(upperLeftCorner.x - 1, bottomRightCorner.y));
-      totalSum -= get2DSum(gridPrefixSum, new Point(upperLeftCorner.y - 1, bottomRightCorner.x));
-      return totalSum;
-   }
+    private int[][] get2DPrefixSum(int[][] grid) {
+        int[][] gridPrefixSum = new int[n + 1][m + 1];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                gridPrefixSum[row + 1][col + 1] = grid[row][col]
+                        + get2DSum(gridPrefixSum, new Point(row, col + 1))
+                        + get2DSum(gridPrefixSum, new Point(row + 1, col))
+                        - get2DSum(gridPrefixSum, new Point(row, col));
+            }
+        }
+        return gridPrefixSum;
+    }
 
-   int get2DSum(int[][] gridPrefixSum, Point point) {
-      if (point.isValid(n, m)) {
-         return gridPrefixSum[point.x][point.y];
-      }
-      return 0;
-   }
+    private boolean isGridEmpty(int[][] gridPrefixSum, Point upperLeftCorner, Point bottomRightCorner) {
+        return get2DSum(gridPrefixSum, upperLeftCorner, bottomRightCorner) == 0;
+    }
 
-   private static class Point {
-      int x, y;
+    int get2DSum(int[][] gridPrefixSum, Point upperLeftCorner, Point bottomRightCorner) {
+        int totalSum = gridPrefixSum[bottomRightCorner.x][bottomRightCorner.y];
+        totalSum -= get2DSum(gridPrefixSum, new Point(upperLeftCorner.x, bottomRightCorner.y));
+        totalSum -= get2DSum(gridPrefixSum, new Point(bottomRightCorner.x, upperLeftCorner.y));
+        return totalSum + gridPrefixSum[upperLeftCorner.x][upperLeftCorner.y];
+    }
 
-      public Point(int x, int y) {
-         this.x = x;
-         this.y = y;
-      }
+    int get2DSum(int[][] gridPrefixSum, Point point) {
+        if (point.isValid(gridPrefixSum.length, gridPrefixSum[0].length)) {
+            return gridPrefixSum[point.x][point.y];
+        }
+        return 0;
+    }
 
-      public boolean isValid(int row, int col) {
-         return x >= 0 && y >= 0 && x < row && y < col;
-      }
-   }
+    private static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean isValid(int row, int col) {
+            return x >= 0 && y >= 0 && x < row && y < col;
+        }
+    }
 }
--- LeetCode/RangeSumQuryImmutable.java
+++ LeetCode/RangeSumQuryImmutable.java
@@ -2,32 +2,32 @@ package LeetCode;
 
 /**
  * Given a 2D matrix matrix, handle multiple queries of the following type:
- *
+ * <p>
  * Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
  * Implement the NumMatrix class:
- *
+ * <p>
  * NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
  * int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
- *
- *
+ * <p>
+ * <p>
  * Example 1:
- *
- *
+ * <p>
+ * <p>
  * Input
  * ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
  * [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
  * Output
  * [null, 8, 11, 12]
- *
+ * <p>
  * Explanation
  * NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
  * numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
  * numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green rectangle)
  * numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue rectangle)
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * m == matrix.length
  * n == matrix[i].length
  * 1 <= m, n <= 200
@@ -39,7 +39,9 @@ package LeetCode;
 public class RangeSumQuryImmutable {
 
     public static void main(String[] args) {
-
+        System.out.println(new RangeSumQuryImmutable(new int[][]{
+                {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}
+        }));
     }
 
     int[][] matrixPrefixSum;
@@ -58,7 +60,6 @@ public class RangeSumQuryImmutable {
                 matrixPrefixSum[i + 1][j + 1] = matrixSum;
             }
         }
-
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
